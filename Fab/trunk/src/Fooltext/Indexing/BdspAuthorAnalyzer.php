82ffcbb03219fbd25beac5963f6b0579dbba5cb8
Daniel Ménard <daniel-menard@users.noreply.github.com>
2012-01-05 10:36:02 +0000

    Plutôt Fab2 que Fab pour éviter toute confusion.

--- Fab/trunk/src/Fooltext/Indexing/BdspAuthorAnalyzer.php
+++
@@ -1,9 +0,0 @@
-<?php
-class AuthorAnalyzer implements AnalyzerInterface
-{
-    public function analyze(AnalyzerData $data)
-    {
-        foreach($data->data as & $value)
-            $value = strtok('/:#', $value);
-    }
-}

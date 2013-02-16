14784df79d1131c2e628cce4b045f37de6ba3cdd
Daniel Ménard <daniel-menard@users.noreply.github.com>
2013-02-16 09:12:44 +0000

    docalist-forms : Thème bootstrap : les attributs n'étaient pas générés pour les checklist.

--- docalist-forms/trunk/src/themes/bootstrap/checklist.widget.php
+++ docalist-forms/trunk/src/themes/bootstrap/checklist.widget.php
@@ -1,5 +1,6 @@
 <?php
 // on est obligé d'avoir un block div englobant si la checklist est répétable
 if ($this->repeatable) $writer->startElement('div');
+$this->render($theme, 'attributes', $args);
 $this->render($theme, 'options', $args);
 if ($this->repeatable) $writer->fullEndElement();

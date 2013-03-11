07d854307ac6318dab5fbb5b08a8e496618349de
Daniel Ménard <daniel-menard@users.noreply.github.com>
2013-03-11 09:56:08 +0000

    docalist-core : Amélioration de la classe Registrable.
    - Gestion plus flexible des ID : si la classe descendante n'a pas définit explicitement un ID, on le construit automatiquement à partir des noms de la classe parent et de la classe en cours.
    - Nouvelle méthode plugin() qui retourne le plugin auquel est rattaché l'objet (i.e. la racine de la hiérarchie)
    - Ré-écriture de la méthode setting() : avec la nouvelle méthode plugin() on a facilement accès à la configuration du plugin.
    - Nouvelle méthode settings() qui retourne la totalité de la conf. C'est plus intuitif que d'appeller setting() sans paramètres.
    - Amélioration de la phpdoc
    - Messages d'erreurs plus explicites.

--- docalist-0core/trunk/class/Registrable.php
+++ docalist-0core/trunk/class/Registrable.php
@@ -10,7 +10,8 @@
  * @package     Docalist
  * @subpackage  Core
  * @author      Daniel Ménard <daniel.menard@laposte.net>
- * @version     SVN: $Id$
+ * @version     SVN: $Id: Registrable.php 444 2013-02-27 14:49:45Z
+ * daniel.menard.35@gmail.com $
  */
 
 namespace Docalist;
@@ -18,7 +19,7 @@ use Exception;
 
 /**
  * Classe de base pour un objet nommé qui peut être ajouté dans un
- * container (post type, taxonomie, settings, admin page, etc.)
+ * container (post type, taxonomie, settings, admin page, settings page, etc.)
  */
 abstract class Registrable {
     /**
@@ -29,12 +30,41 @@ abstract class Registrable {
     /**
      * @var string L'identifiant unique de cet objet.
      *
-     * Cette propriété doit être initialisée par les classes descendantes.
+     * La majorité des objets enregistrables sont capables de construire
+     * automatiquement leur id en combinant le nom de leur classe avec le
+     * nom de leur container (cf. {@link id()}).
+     *
+     * Dans certains cas, il est nécessaire de définir explicitement un id
+     * pour que celui-ci puisse être facilement utilisé de façon transversale
+     * (par exemple le nom d'un custom post type ou d'une taxonomie).
+     *
+     * Dans ce cas, les classes descendantes peuvent redéfinir cette propriété
+     * ou l'initialiser dans leur constructeur.
+     *
+     * La méthode {@link id()} retournera alors l'id indiqué.
      */
     protected $id;
 
     /**
+     * Initialise et enregistre l'objet dans WordPress.
+     */
+    abstract public function register();
+
+    /**
      * Retourne ou modifie le container de cet objet.
+     *
+     * Appellée sans paramètre, la méthode retourne le container parent
+     * de l'objet ou null si l'objet n'a pas de parent.
+     *
+     * Utilisée comme Setter, elle ajoute l'objet dans le containeur passé
+     * en paramètre.
+     *
+     * Remarque :
+     *
+     * On ne peut pas modifier le parent d'un objet Registrable : une fois
+     * qu'un objet a été ajouté à un container (il a un parent), il ne peut
+     * plus être ajouté à un autre objet. Une exception sera levée si on
+     * essaie de modifier le parent d'un objet qui a déjà un parent.
      *
      * @param Container $container Le container de l'objet.
      *
@@ -43,20 +73,38 @@ abstract class Registrable {
      * @return Container|$this
      */
     public function parent(Container $parent = null) {
-        // Setter
-        if ($parent) {
-            if (! is_null($this->parent)) {
-                $msg = __("L'objet %s a déjà un container parent", 'docalist-core');
-                throw new Exception(sprintf($msg, $this->name()));
-            }
-
-            $this->parent = $parent;
-
-            return $this;
+        // Getter
+        if (is_null($parent)) {
+            return $this->parent;
         }
 
-        // Getter
-        return $this->parent;
+        // Setter
+        if (!is_null($this->parent)) {
+            $msg = __("L'objet %s a déjà un parent", 'docalist-core');
+            throw new Exception(sprintf($msg, $this->name()));
+        }
+
+        $this->parent = $parent;
+
+        return $this;
+    }
+
+    /**
+     * Retourne le plugin auquel est rattaché cet objet.
+     *
+     * La méthode se contente d'appeller la méthode plugin() de son container.
+     *
+     * @throws Exception Si l'objet n'a pas de parent.
+     *
+     * @return AbstractPlugin
+     */
+    public function plugin() {
+        if (!$this->parent) {
+            $msg = __("L'objet %s n'a pas de parent", 'docalist-core');
+            throw new Exception(sprintf($msg, $this->name()));
+        }
+
+        return $this->parent->plugin();
     }
 
     /**
@@ -64,11 +112,22 @@ abstract class Registrable {
      *
      * Pour une taxonomie ou un post-type, cela correspond au code utilisé en
      * interne par WordPress. Pour des options de configuration, cela
-     * correspond à la clé utilisée dans la table wp_options. Etc.
+     * correspond à la clé utilisée dans la table wp_options, etc.
      *
      * @return string
      */
     public function id() {
+        // Si la classe descendante a explicitement définit un id, on le prend
+        if (isset($this->id)) {
+            return $this->id;
+        }
+
+        // Sinon, on le construit (et on le stocke pour la prochaine fois)
+        $this->id = $this->name();
+        if ($this->parent) {
+            $this->id = $this->parent->id() . '-' . $this->id;
+        }
+
         return $this->id;
     }
 
@@ -80,34 +139,51 @@ abstract class Registrable {
      *
      * @return string
      */
-    public final function name() {
+    public function name() {
         return strtolower(Utils::classname($this));
     }
 
+    /**
+     * Retourne la configuration actuelle du plugin.
+     *
+     * Tous les objets Registrable ont accès à la configuration du plugin
+     * auquel ils sont ratttachés.
+     *
+     * A tout niveau (post type, metabox, settingspage, etc.) on peut ainsi
+     * appeller $this->setting(option) ou $this->settings().
+     *
+     * @return array
+     */
+    public function settings() {
+        return $this->plugin()->settings();
+    }
 
     /**
-     * Initialise et enregistre l'objet dans WordPress.
+     * Retourne la valeur actuelle d'une option de configuration.
+     *
+     * Vous pouvez utiliser des points dans les noms d'options pour
+     * accéder directement à une sous-option.
+     *
+     * Par exemple :
+     * <code>$this->setting('options.display.contrast')</code>
+     *
+     * est équivalent à
+     *
+     * <code>
+     * $settings = $this->settings();
+     * $settings['options']['display']['constrast']
+     * </code>
+     *
+     * mais ne générera aucune erreur si l'un des noms indiqués n'existe pas.
+     *
+     * @param string le nom de l'option à retourner.
+     *
+     * @return array|scalar|null Retourne un tableau si $setting désigne un
+     * groupe d'options, un scalaire si $options est une option et null si la
+     * clé demandée n'existe pas.
      */
-    abstract public function register();
-
-    /**
-     * Retourne la valeur d'une option de configuration du plugin.
-     *
-     * Si aucun paramètre n'est fourni, la méthode retourne un tableau
-     * contenant toutes les options.
-     *
-     * @param string|null Le nom de l'option à retourner.
-     * @param mixed $default La valeur à retourner si l'option n'existe pas
-     * (null par défaut).
-     *
-     * @return mixed
-     */
-    public function setting($option = null, $default = null) {
-        static $settings;
-
-        is_null($settings) && $settings = $this->parent->get('settings');
-
-        return $settings->get($option, $default);
+    public function setting($setting) {
+        return $this->plugin()->setting($setting);
     }
 
 }

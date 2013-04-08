6b43ea2348742a77867dee46685cfedddc38447f
Daniel Ménard <daniel-menard@users.noreply.github.com>
2013-04-08 10:01:01 +0000

    Uri : Nouvelle classe (extends QueryString) pour gérer des URLS. Coquille vide pour l'instant, à finaliser.

---
+++ docalist-0core/trunk/class/Uri.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * This file is part of the "Docalist Core" plugin.
+ *
+ * Copyright (C) 2012, 2013 Daniel Ménard
+ *
+ * For copyright and license information, please view the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * @package     Docalist
+ * @subpackage  Core
+ * @author      Daniel Ménard <daniel.menard@laposte.net>
+ * @version     SVN: $Id$
+ */
+
+namespace Docalist;
+
+/**
+ * Permet de manipuler facilement une Url.
+ */
+class Uri extends QueryString {
+    // TODO: Uri
+
+    // scheme, host, port, path, fragment
+}

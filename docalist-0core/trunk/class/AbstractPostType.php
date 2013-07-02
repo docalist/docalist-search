17e873b6599317e00ed2e8a56acda3612513f579
Daniel Ménard <daniel-menard@users.noreply.github.com>
2013-07-02 08:36:38 +0000

    Renomme la classe PostType en AbstractPostType pour que ce soit cohérent avec le reste.

--- docalist-0core/trunk/class/AbstractPostType.php
+++ docalist-0core/trunk/class/AbstractPostType.php
@@ -23,7 +23,7 @@ use Docalist\Forms\Assets, Docalist\Forms\Themes;
  *
  * C'est un Container qui gère les metaboxes associées.
  */
-abstract class PostType implements ContainerInterface {
+abstract class AbstractPostType implements ContainerInterface {
     use ContainerTrait;
 
     /*

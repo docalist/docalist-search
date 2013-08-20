52bf38920bc31ddef189540f3d5eb4f7c16161be
Daniel Ménard <daniel-menard@users.noreply.github.com>
2013-08-20 21:42:10 +0000

    Chargement des plugins : remarques (en commentaires) sur la façon dont cela pourrait être implémenté dans une prochaine version.

--- docalist-0core/trunk/docalist-core.php
+++ docalist-0core/trunk/docalist-core.php
@@ -40,3 +40,56 @@ Autoloader::register('Docalist\Forms', __DIR__ . '/lib/docalist-forms/class');
 
 // Charge le plugin "Core"
 Docalist::load('Docalist\Core', __FILE__);
+
+/*
+
+A revoir dans une nouvelle version.
+Objectifs :
+- Se débarrasser des classes statiques.
+- Etre sur que docalist-core est chargé en premier, sans avoir le truc du 0-core
+- Que les plugins docalist ne plantent pas si docalist core est désactivé
+
+Principe :
+- wordpress charge tous les plugins
+- ceux-ci ne font rien : ils se contentent d'installer un filtre wordpress qui
+  sera appellé quand docalist core sera chargé
+
+Dans docalist core :
+namespace Docalist\Core;
+
+add_action('plugins_loaded', function() {
+    // Initialise l'autoloader
+    require_once __DIR__ . '/class/Autoloader.php';
+    $autoloader = new Autoloader();
+
+    // Enregistre notre espace de nom
+    $autoloader->register(__NAMESPACE__, __DIR__ . '/class');
+
+    // Charge le plugin
+    $docalist = new Plugin();
+
+    do_action('docalist_loaded', $docalist);
+});
+
+Eventuellement, stocker $docalist dans une var globale ?
+Et avoir une fonction générique :
+function docalist($key) {
+    global $docalist;
+
+    return $key ? $docalist->get($key) : $docalist;
+}
+
+Dans un plugin :
+
+add_action('docalist_loaded', function(Docalist\Core\Plugin $docalist) {
+    // Enregistre notre espace de noms
+    $docalist->autoload(__NAMESPACE__, __DIR__ . '/class');
+
+    // Charge le plugin
+    $docalist->add(new Plugin)
+});
+
+// $docalist->autoload(xxx) est juste un raccourci vers
+// $docalist->autoloader()->register(xxx)
+
+*/

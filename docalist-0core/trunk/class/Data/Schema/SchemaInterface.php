229f14c5da6fbbcd5407ae2c087886c6ab492ff9
Daniel Ménard <daniel-menard@users.noreply.github.com>
2013-08-20 20:08:39 +0000

    SchemaInterface : interface qui permet de décrire la liste des champs qui composent une structure de données.

---
+++ docalist-0core/trunk/class/Data/Schema/SchemaInterface.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * This file is part of a "Docalist Core" plugin.
+ *
+ * For copyright and license information, please view the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * @package Docalist
+ * @subpackage Core
+ * @author Daniel Ménard <daniel.menard@laposte.net>
+ * @version SVN: $Id$
+ */
+namespace Docalist\Data\Schema;
+
+/**
+ * L'interface Schema permet de décrire la liste des champs qui composent une
+ * structure de données.
+ */
+interface SchemaInterface {
+
+    /**
+     * Retourne le nom.
+     *
+     * @return string
+     */
+    public function name();
+
+    /**
+     * Retourne le type.
+     *
+     * @return string
+     */
+    public function type();
+
+    /**
+     * Retourne la liste des champs.
+     *
+     * @return FieldInterface[]
+     */
+    public function fields();
+
+    /**
+     * Retourne le schéma du champ indiqué.
+     *
+     * @param string $field Le nom du champ.
+     *
+     * @throws Exception Une exception est générée si le champ indiqué n'existe
+     * pas.
+     *
+     * @return FieldInterface
+     */
+    public function field($field);
+
+    public function toArray();
+}

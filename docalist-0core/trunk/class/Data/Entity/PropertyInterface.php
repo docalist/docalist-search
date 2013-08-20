b4ee961f427750c4c34dbf9c208273e84ecca4f4
Daniel Ménard <daniel-menard@users.noreply.github.com>
2013-08-20 20:13:23 +0000

    PropertyInterface : interface d'une propriété structurée ou répétable au sein d'une entité.

---
+++ docalist-0core/trunk/class/Data/Entity/PropertyInterface.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * This file is part of a "Docalist Core" plugin.
+ *
+ * For copyright and license information, please view the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * @package Docalist
+ * @subpackage Core
+ * @author Daniel Ménard <daniel.menard@laposte.net>
+ * @version SVN: $Id$
+ */
+namespace Docalist\Data\Entity;
+
+/**
+ * Interface d'une propriété au sein d'une entité.
+ */
+interface PropertyInterface extends SchemaBasedInterface {
+}

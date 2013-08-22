73377c0926d741104ef50a58058605fbf5334b89
Daniel Ménard <daniel-menard@users.noreply.github.com>
2013-08-22 18:44:52 +0000

    Nouvelle implémentation de Property basée sur SchemaBasedInterface et non plus sur ArrayObject.

--- docalist-0core/trunk/class/Data/Entity/Property.php
+++ docalist-0core/trunk/class/Data/Entity/Property.php
@@ -28,7 +28,7 @@ use Docalist\Utils;
  * Les propriétés scalaires simples (non répétables) sont directement
  * représentée par le type php correspondant.
  */
-class Property extends ArrayObject implements PropertyInterface {
+class Property extends SchemaBasedObject implements PropertyInterface {
     /**
      * Le schéma de la propriété.
      *
@@ -43,130 +43,14 @@ class Property extends ArrayObject implements PropertyInterface {
      *
      * @param array $data Les données initiales de la propriété.
      */
-    public function __construct(FieldInterface $schema, array $data) {
+    public function __construct(FieldInterface $schema, array $data = null) {
         // Stocke le schéma
         $this->schema = $schema;
 
-        // Configure l'objet ArrayAccess
-        if (! $schema->repeatable()) {
-            parent::setFlags(self::ARRAY_AS_PROPS);
-        }
-
-        // Stocke les données en appellant offsetSet pour chaque champ
-        if (!is_null($data)) {
-            foreach ($data as $field => $value) {
-                $this->offsetSet($field, $value);
-            }
-        }
+        parent::__construct($data);
     }
 
     public function schema($field = null) {
         return is_null($field) ? $this->schema : $this->schema->field($field);
     }
-
-    public function offsetGet($key) {
-        // Si le champ est répétable, on fonctionne comme un tableau, key doit être un entier
-        if ($this->schema->repeatable()) {
-            if (! is_int($key)) {
-                $msg = 'Bad index %s, expected int';
-                throw new InvalidArgumentException(sprintf($msg, $key));
-            }
-
-            if (!$this->offsetExists($key)) {
-                $msg = 'Index %s does not exists';
-                throw new InvalidArgumentException(sprintf($msg, $key));
-
-            }
-
-            // Retourne le key-ème élément
-            return parent::offsetGet($key);
-        }
-
-        // Objet simple, key doit être un nom de champ
-        else {
-            if (! is_string($key)) {
-                $msg = 'Bad field name %s, expected string';
-                throw new InvalidArgumentException(sprintf($msg, $key));
-            }
-
-            // Vérifie que le champ existe et récupère son schéma
-            $field = $this->schema->field($key);
-
-            // Si le champ n'est pas initialisé, stocke sa valeur par défaut
-            if (!$this->offsetExists($key)) {
-                parent::offsetSet($key, $field->instantiate($field->defaultValue()));
-            }
-
-            // Retourne la valeur
-            return parent::offsetGet($key);
-        }
-    }
-
-    /**
-     * Modifie la valeur du champ indiqué.
-     *
-     * @param string $name
-     * @param string $value
-     *
-     * @throws Exception Si le champ indiqué n'existe pas dans le schéma.
-     */
-    public function offsetSet($key, $value) {
-        // Si le champ est répétable, on fonctionne comme un tableau, key doit être un entier
-        if ($this->schema->repeatable()) {
-            if (! is_int($key)) {
-                $msg = 'Bad index %s, expected int';
-                throw new InvalidArgumentException(sprintf($msg, $key));
-            }
-            $field = $this->schema;
-            $single = true;
-        }
-
-        // Objet simple, key doit être un nom de champ
-        else {
-            if (! is_string($key)) {
-                $msg = 'Bad field name %s, expected string';
-                throw new InvalidArgumentException(sprintf($msg, $key));
-            }
-
-            // Vérifie que le champ existe et récupère son schéma
-            $field = $this->schema($key);
-            $single = false;
-        }
-
-        // Attribuer null à un champ équivaut à unset()
-        if ($value === null) {
-            parent::offsetUnset($key);
-
-            return;
-        }
-
-        // Stocke la valeur
-        parent::offsetSet($key, $field->instantiate($value, $single));
-    }
-
-
-//     /**
-//      * Retourne un tableau contenant les données actuelles de la propriété.
-//      *
-//      * @return array
-//      */
-//     public function toArray() {
-//         $data = $this->getArrayCopy(); // $this ?
-//         foreach ($data as $name => $value) {
-//             if (is_scalar($value)) {
-//                 $field = $this->schema($name);
-//                 if ($value === $field->defaultValue()) {
-//                     unset($data[$name]);
-//                 }
-//             } else {
-//                 if (count($value) === 0) {
-//                     unset($data[$name]);
-//                 } else {
-//                     $data[$name] = $value->toArray();
-//                 }
-//             }
-//         }
-
-//         return $data;
-//     }
 }

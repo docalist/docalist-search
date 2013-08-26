48880c0428a8b873690a530b78950c080d8259b6
Daniel Ménard <daniel-menard@users.noreply.github.com>
2013-08-26 10:28:29 +0000

    Collection : mise à jour/suppression de phpdoc (copier/coller de la doc de l'interface).

--- docalist-0core/trunk/class/Data/Entity/Collection.php
+++ docalist-0core/trunk/class/Data/Entity/Collection.php
@@ -20,40 +20,35 @@ use ArrayAccess;
 use ArrayIterator;
 
 /**
- * Implémentation standard de l'interface PropertyInterface.
- *
- * Cette classe permet d'encapsuler :
- * - une propriété scalaire répétable (int[], string[], ...)
- * - un objet anonyme (sans entité associée)
- * - une collection d'objets anonymes (object[])
- * - une collection d'entités
- *
- * Les propriétés scalaires simples (non répétables) sont directement
- * représentée par le type php correspondant.
- *
- * @todo : faire une interface CollectionInterface (pour les tests, entre autres)
+ * Une propriété répétable.
  */
 class Collection implements SchemaBasedObjectInterface, ArrayAccess {
     /**
-     * Le schéma de la propriété.
+     * Le schéma des éléements de la collection.
      *
      * @var FieldInterface
      */
     protected $schema;
 
+    /**
+     * Les données de la collection
+     *
+     * @var scalar|Property
+     */
     protected $items = array();
 
     /**
-     * Construit une nouvelle propriété.
+     * Construit une nouvelle collection.
      *
-     * @param FieldInterface $schema Le schéma de la propriété.
+     * @param FieldInterface $schema Le schéma de la collection.
      *
-     * @param array $data Les données initiales de la propriété.
+     * @param array $data Les données initiales de la collection.
      */
     public function __construct(FieldInterface $schema, array $data = null) {
         // Stocke le schéma
         $this->schema = $schema;
 
+        // Stocke les données
         if (! is_null($data)) {
             foreach($data as $item) {
                 $this->items[] = $schema->instantiate($item, true);
@@ -65,56 +60,18 @@ class Collection implements SchemaBasedObjectInterface, ArrayAccess {
         return is_null($field) ? $this->schema : $this->schema->field($field);
     }
 
-    /**
-     * Retourne le nombre de propriétés.
-     *
-     * Cette méthode fait partie de l'interface {@link Countable} qui permet
-     * d'utiliser la fonction count().
-     *
-     * La méthode retourne le nombre de champs qui sont renseignés au sein de
-     * l'objet (contrairement à la méthode count de la classe {@link Schema}
-     * qui retourne le nombre de champs définis).
-     */
     public function count() {
         return count($this->items);
     }
 
-
-    // Interface IteratorAggregate
-
-    /**
-     * Retourne un itérateur permettant d'énumérer les propriétés de l'objet.
-     *
-     * Cette méthode fait partie de l'interface {@link IteratorAggregate} qui
-     * permet d'utiliser l'objet dans une boucle foreach.
-     *
-     * @return Iterator
-    */
     public function getIterator() {
         return new ArrayIterator($this->items);
     }
 
-    // Interface Serializable
-
-    /**
-     * Retourne une chaine contenant la version sérialisée de l'objet.
-     *
-     * Cette méthode fait partie de l'interface {@link Serializable}.
-     *
-     * @return string
-    */
     public function serialize() {
         return serialize($this->items);
     }
 
-    /**
-     * Construit un nouvel objet à partir des données sérialisées passées
-     * en paramètre.
-     *
-     * Cette méthode fait partie de l'interface {@link Serializable}.
-     *
-     * @param serialized
-    */
     public function unserialize($serialized) {
         $this->items = unserialize($serialized);
     }
@@ -126,9 +83,11 @@ class Collection implements SchemaBasedObjectInterface, ArrayAccess {
     public function offsetExists ($offset) {
         return isset($this->items[$offset]);
     }
+
     public function offsetGet ($offset) {
         return $this->items[$offset];
     }
+
     public function offsetSet ($offset, $value) {
         if (is_null($offset)) {
             $this->items[] = $this->schema->instantiate($value, true);
@@ -140,5 +99,4 @@ class Collection implements SchemaBasedObjectInterface, ArrayAccess {
     public function offsetUnset($offset) {
         $this->schema->instantiate(null, true);
     }
-
 }

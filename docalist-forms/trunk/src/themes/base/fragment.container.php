1844a4052f92de6b13d3d6283add73ae25d9b077
Daniel Ménard <daniel-menard@users.noreply.github.com>
2013-08-27 10:32:48 +0000

    fragmet.container : utilise les getters plutôt que les propriétés.

--- docalist-forms/trunk/src/themes/base/fragment.container.php
+++ docalist-forms/trunk/src/themes/base/fragment.container.php
@@ -1,10 +1,10 @@
 <?php
-$this->label && $this->block('label');
-$this->description && (! $this->descriptionAfter) && $this->block('description');
+$this->label() && $this->block('label');
+$this->description() && (! $this->descriptionAfter) && $this->block('description');
 $this->block('errors');
 
 foreach($this->fields as $field) {
     $field->block('container');
 }
 
-$this->description && $this->descriptionAfter && $this->block('description');
+$this->description() && $this->descriptionAfter && $this->block('description');

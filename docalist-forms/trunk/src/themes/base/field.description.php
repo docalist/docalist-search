c8138c264b826d3b5f3a06ae4debbf040880d961
Daniel Ménard <daniel-menard@users.noreply.github.com>
2013-08-27 10:32:21 +0000

    field.description : description() et non pas $description.

--- docalist-forms/trunk/src/themes/base/field.description.php
+++ docalist-forms/trunk/src/themes/base/field.description.php
@@ -1,5 +1,5 @@
 <?php
 $writer->startElement('p');
 $writer->writeAttribute('class', $this->descriptionAfter ? 'description' : 'description after');
-$writer->writeRaw($this->description);
+$writer->writeRaw($this->description());
 $writer->fullEndElement();

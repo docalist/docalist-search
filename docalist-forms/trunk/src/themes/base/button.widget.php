3774b7ba7ea9608c3dd0462023b9b43e32701c52
Daniel Ménard <daniel-menard@users.noreply.github.com>
2013-08-27 10:29:45 +0000

    base/button.widget : utilise les getters plutôt que les propriétés.

--- docalist-forms/trunk/src/themes/base/button.widget.php
+++ docalist-forms/trunk/src/themes/base/button.widget.php
@@ -1,6 +1,6 @@
 <?php
 $writer->startElement('button');
-$this->name && $writer->writeAttribute('name', $this->controlName());
+$this->name() && $writer->writeAttribute('name', $this->controlName());
 $this->block('attributes', $args);
-$this->label && $writer->text($this->label);
+if ($label = $this->label()) $writer->text($label);
 $writer->fullEndElement();

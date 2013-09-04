3b0bc9681adea9aa5feabc1237fcdb14b4d3a175
Daniel Ménard <daniel-menard@users.noreply.github.com>
2013-09-04 16:50:29 +0000

    Settings : ré-écriture complète sous forme d'entité.

--- class/Settings.php
+++ class/Settings.php
@@ -7,44 +7,46 @@
  * For copyright and license information, please view the
  * LICENSE.txt file that was distributed with this source code.
  *
- * @package     Docalist
- * @subpackage  Search
- * @author      Daniel Ménard <daniel.menard@laposte.net>
- * @version     SVN: $Id$
+ * @package Docalist
+ * @subpackage Search
+ * @author Daniel Ménard <daniel.menard@laposte.net>
+ * @version SVN: $Id$
  */
 namespace Docalist\Search;
-use Docalist\AbstractSettings;
+
+use Docalist\Data\Entity\AbstractSettingsEntity;
 
 /**
  * Options de configuration du plugin.
+ *
+ * @property bool $enabled Indique si la recherche est activée.
+ * @property ServerSettings $server Paramètres du serveur ElasticSearch.
+ * @property IndexerSettings $indexer Paramètres de l'indexeur.
+ * @property string[] $types Contenus à indexer.
  */
-class Settings extends AbstractSettings {
-    /**
-     * {@inheritdoc}
-     */
-    protected $defaults = array(
-        // Paramètres généraux
-        'general' => array(
-            // activer la recherche
-            'enabled' => false, ),
+class Settings extends AbstractSettingsEntity {
 
-        // Paramètres du serveur elasticsearch
-        'server' => array(
-            // Url du serveur
-            'url' => 'http://127.0.0.1:9200/',
-
-            // Nom de l'index ES à créer
-            'index' => 'wordpress',
-
-            // Timeout des requêtes
-            'timeout' => 10,
-        ),
-
-        // Contenus à indexer
-        'content' => array(
-            'posttypes' => array(),
-            'comments' => false,
-            'users' => false,
-        ),
-    );
+    protected function loadSchema() {
+        // @formatter:off
+        return array(
+            'enabled' => array(
+                'type' => 'bool',
+                'label' => __('Activer la recherche', 'docalist-search'),
+                'default' => false,
+            ),
+            'server' => array(
+                'label' => __('Serveur elasticsearch', 'docalist-search'),
+                'type' => 'ServerSettings',
+            ),
+            'indexer' => array(
+                'label' => __("Paramètres de l'indexeur", 'docalist-search'),
+                'type' => 'IndexerSettings',
+            ),
+            'types' => array(
+                'label' => __('Contenus à indexer', 'docalist-search'),
+                'type' => 'string*',
+            ),
+        );
+        // @formatter:on
+    }
 }

4149651dd3b68ad76e553d84d1ac3c98ab6a2d8a
Daniel Ménard <daniel-menard@users.noreply.github.com>
2013-10-23 10:31:03 +0000

    Request : wrapper autour de l'objet Request de Symfony (non utilisé pour le moment).

---
+++ docalist-0core/trunk/class/Http/Request.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * This file is part of the 'Docalist Core' plugin.
+ *
+ * Copyright (C) 2012, 2013 Daniel Ménard
+ *
+ * For copyright and license information, please view the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * @package     Docalist
+ * @subpackage  Response
+ * @author      Daniel Ménard <daniel.menard@laposte.net>
+ * @version     $Id$
+ */
+namespace Docalist\Http;
+
+use Symfony\Component\HttpFoundation\Request as SymfonyRequest;
+
+class Request extends SymfonyRequest {
+}

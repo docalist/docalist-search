b32323469533868a17768ced5de506f959f80e19
Daniel Ménard <daniel-menard@users.noreply.github.com>
2013-10-23 12:38:33 +0000

    Vue Info : affiche un message d'information.

---
+++ docalist-0core/trunk/views/info.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * This file is part of the 'Docalist Core' plugin.
+ *
+ * Copyright (C) 2012, 2013 Daniel Ménard
+ *
+ * For copyright and license information, please view the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * @package     Docalist
+ * @subpackage  Core
+ * @author      Daniel Ménard <daniel.menard@laposte.net>
+ * @version     $Id$
+ */
+namespace Docalist;
+
+/**
+ * Affiche un message d'information.
+ */
+
+/* @var $h2 string */
+/* @var $h3 string */
+/* @var $message string */
+/* @var $back string */
+
+! isset($h2) && $h2 = __('Information', 'docalist-core');
+$href = isset($back) ? esc_url($back) : 'javascript:history.go(-1)'
+?>
+
+<div class="wrap">
+
+<?= screen_icon() ?>
+<h2><?= $h2 ?></h2>
+
+<div class="updated">
+    <?php if (isset($h3)) :?>
+        <h3><?= $h3 ?></h3>
+    <?php endif ?>
+
+    <?php if (isset($message)) :?>
+        <p><?= $message ?></p>
+    <?php endif ?>
+
+    <p>
+        <a href="<?= $href ?>" class="button-primary">
+            <?= __('Ok', 'docalist-core') ?>
+        </a>
+    </p>
+</div>
+
+</div>

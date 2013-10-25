95406c5520e12fc3d8a90c7471e83c8276950ade
Daniel Ménard <daniel-menard@users.noreply.github.com>
2013-10-25 17:07:39 +0000

    Ajout de la vue 'actions-list' qui génère la liste des actions disponibles dans un module (action par défaut d'une page).

---
+++ docalist-0core/trunk/views/controller/actions-list.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * This file is part of the 'Docalist Core' plugin.
+ *
+ * Copyright (C) 2012, 2013 Daniel Ménard
+ *
+ * For copyright and license information, please view the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * @package     Docalist
+ * @subpackage  Core
+ * @author      Daniel Ménard <daniel.menard@laposte.net>
+ * @version     $Id$
+ */
+namespace Docalist;
+
+/**
+ * Liste les actions disponibles dans un contrôleur.
+ *
+ * @param string $title Titre de la page
+ * @param string[] $actions Liste des actions à affficher.
+ */
+?>
+<div class="wrap">
+    <?= screen_icon() ?>
+    <h2><?= $title ?></h2>
+
+    <?php if (empty($actions)): ?>
+        <p><?= __("Aucune action n'est disponible dans ce module.", 'docalist-core') ?></p>
+    <?php else: ?>
+        <ul class="ul-disc">
+        <?php foreach($actions as $action): ?>
+            <li>
+                <h3>
+                    <a href="<?= $this->url($action) ?>"><?= $action ?></a>
+                </h3>
+            </li>
+        <?php endforeach; ?>
+        </ul>
+    <?php endif; ?>
+
+</div>

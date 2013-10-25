5ee76e38a09d5041b933bbf19056ab5423c3fd32
Daniel Ménard <daniel-menard@users.noreply.github.com>
2013-10-25 17:03:01 +0000

    Ajout de la vue 'access-denied' qui génère une page "accès refusé" si l'utilisateur n'a pas les droits requis pour exécuter une action.

---
+++ docalist-0core/trunk/views/controller/access-denied.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * This file is part of the 'Docalist Core' plugin.
+ *
+ * Copyright (C) 2012, 2013 Daniel Ménard
+ *
+ * For copyright and license information, please view the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * @package     Docalist
+ * @subpackage  Core
+ * @author      Daniel Ménard <daniel.menard@laposte.net>
+ * @version     $Id$
+ */
+namespace Docalist;
+
+/**
+ * Génère une page "Accès non autorisé".
+ *
+ * @param string $action Le nom de l'action exécutée.
+ */
+
+$title = __('Accès refusé', 'docalist-core');
+$message = __("Désolé, mais vous n'avez pas les droits requis pour exécuter cette action.", 'docalist-core');
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title><?= $title ?></title>
+    </head>
+    <body>
+        <h1><?= $title ?></h1>
+        <p><?= $message ?></p>
+    </body>
+</html>

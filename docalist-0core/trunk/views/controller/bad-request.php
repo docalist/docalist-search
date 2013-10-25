22986b9e8695aa604b0a168085ec03c4c7feb03d
Daniel Ménard <daniel-menard@users.noreply.github.com>
2013-10-25 17:03:43 +0000

    Ajout de la vue 'bad-request' qui génère une erreur si une action n'est pas appellée correctement (paramètres manquants par exemple).

---
+++ docalist-0core/trunk/views/controller/bad-request.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * This file is part of the 'Docalist Core' plugin.
+ *
+ * Copyright (C) 2012, 2013 Daniel Ménard
+ *
+ * For copyright and license information, please view the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * @package     Docalist
+ * @subpackage  Core
+ * @author      Daniel Ménard <daniel.menard@laposte.net>
+ * @version     $Id$
+ */
+namespace Docalist;
+
+/**
+ * Génère une page "Bad Request".
+ *
+ * @param string $message Le message à afficher.
+ */
+
+$title = __('Requête incorrecte', 'docalist-core');
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title><?= $title ?></title>
+    </head>
+    <body>
+        <h1><?= $title ?></h1>
+        <p><?= $message ?></p>
+    </body>
+</html>

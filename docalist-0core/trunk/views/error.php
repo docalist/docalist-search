aee1bf93ef848b44861cf40b3fa6efe3cb94d9c4
Daniel Ménard <daniel-menard@users.noreply.github.com>
2013-10-25 17:19:54 +0000

    Vue 'error' : meilleure documentation des données attendues par la vue + h3 optionnel.

--- docalist-0core/trunk/views/error.php
+++ docalist-0core/trunk/views/error.php
@@ -16,36 +16,34 @@ namespace Docalist;
 
 /**
  * Affiche un message d'erreur
+ *
+ * @param string $h2 Titre de la page (optionnel).
+ * @param string $h3 Titre de la boite de confirmation (optionnel).
+ * @param string $message Message à afficher.
+ * @param string $back Url du lien 'annuler' (optionnel)
  */
-
-/* @var $h2 string */
-/* @var $h3 string */
-/* @var $message string */
-/* @var $back string */
-
 ! isset($h2) && $h2 = __('Erreur', 'docalist-core');
+! isset($h3) && $h3 = __('Une erreur est survenue', 'docalist-core');
 $href = isset($back) ? esc_url($back) : 'javascript:history.go(-1)'
 ?>
 
 <div class="wrap">
+    <?= screen_icon() ?>
+    <h2><?= $h2 ?></h2>
 
-<?= screen_icon() ?>
-<h2><?=$h2 ?></h2>
+    <div class="error">
+        <?php if (isset($h3)) :?>
+            <h3><?= $h3 ?></h3>
+        <?php endif ?>
 
-<div class="error">
-    <?php if (isset($h3)) :?>
-        <h3><?= $h3 ?></h3>
-    <?php endif ?>
+        <?php if (isset($message)) :?>
+            <p><?= $message ?></p>
+        <?php endif ?>
 
-    <?php if (isset($message)) :?>
-        <p><?= $message ?></p>
-    <?php endif ?>
-
-    <p>
-        <a href="<?=$href?>" class="button-primary">
-            <?= __('Ok', 'docalist-core') ?>
-        </a>
-    </p>
-</div>
-
+        <p>
+            <a href="<?= $href ?>" class="button-primary">
+                <?= __('Ok', 'docalist-core') ?>
+            </a>
+        </p>
+    </div>
 </div>

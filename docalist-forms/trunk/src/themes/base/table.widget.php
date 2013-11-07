f6d355bb764783d8da9aaf3ad1a8b7962d0ec8d1
Daniel Ménard <daniel-menard@users.noreply.github.com>
2013-11-07 11:19:22 +0000

    forms/themes : supprime style inline qui forçait les input à avoir une largeur de 100% et passe la classe .field-table en "layout fixed".
    Si on utilise le widget "table", on doit fournir la largeur des colonnes (via une classe qui sera ppliquée au th, par exemple).

--- docalist-forms/trunk/src/themes/base/table.widget.php
+++ docalist-forms/trunk/src/themes/base/table.widget.php
@@ -9,7 +9,7 @@ foreach($this->fields as $field) {
 
     $field->removeClass('span1 span2 span3 span4 span5 span6 span7 span8 span9 span10 span11 span12');
     //$field->addClass('input-block-level');
-    $field->attribute('style','width:100%');
+    //$field->attribute('style','width:100%');
 
     $field->block('values');
     $writer->fullEndElement(); // </td>

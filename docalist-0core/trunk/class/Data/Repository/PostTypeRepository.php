c962c4f5900df1fa6cab010a67446e1dc3a929b8
Daniel Ménard <daniel-menard@users.noreply.github.com>
2013-11-08 07:25:42 +0000

    Gestion de la numérotation des notices.
    Mise en place d'un mécanisme permettant de numéroter séquentiellement à partir de 1 les notices d'une base (chaque base a sa propre numérotation).
    
    - Ajout des méthodes sequenceName(), sequenceIncrement(), sequenceClear() et sequenceSetIfGreater() qui permettent de gérer un champ style auto_number.
    
    - deleteAll() : appelle sequenceClear() pour réinitialiser la séquence.
    
    - store() : appelle synchronizePosr() avant d'encoder les données de la notice (et non pas après), afin de permettre aux classes descendantes de modifier l'entité avant qu'elle ne soit sérialisée (par exemple pour initialiser le champ Ref avec une séquence).

--- docalist-0core/trunk/class/Data/Repository/PostTypeRepository.php
+++ docalist-0core/trunk/class/Data/Repository/PostTypeRepository.php
@@ -157,6 +157,11 @@ class PostTypeRepository extends AbstractRepository {
             $post = new WP_Post(new StdClass());
         }
 
+        // Synchronise le post wp à partir des données de l'entité
+        // Permet également de modifier l'entité avant enregistrement (par
+        // exemple, affecter une valeur au champ Ref).
+        $this->synchronizePost($post, $entity);
+
         // Encode les données de l'entité en JSON
         $options = JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE;
         WP_DEBUG && $options |= JSON_PRETTY_PRINT;
@@ -164,9 +169,6 @@ class PostTypeRepository extends AbstractRepository {
 
         // Stocke le JSON dans le champ post_excerpt
         $post->post_excerpt = $data;
-
-        // Synchronise le post wp à partir des données de l'entité
-        $this->synchronizePost($post, $entity);
 
         // Pour wpdb, il faut maintenant un tableau et non plus un WP_Post
         $post = (array) $post;
@@ -211,6 +213,153 @@ class PostTypeRepository extends AbstractRepository {
     public function deleteAll() {
         global $wpdb;
 
-        return $wpdb->delete($wpdb->posts, array('post_type' => $this->postType));
+        // Supprime tous les enregs
+        $nb = $wpdb->delete($wpdb->posts, array('post_type' => $this->postType));
+
+        // Réinitialise les séquences éventuelles utilisées par cette base
+        $this->sequenceClear();
+
+        // Retourne le nombre de notices supprimées
+        return $nb;
+    }
+
+    /**
+     * Méthode utilitaire permettant aux classes descendantes d'implémenter
+     * un champ séquence (typiquement : le champ ref des notices).
+     *
+     * Cette méthode gère une option de la forme "{posttype}_last_{champ}" (par
+     * exemple "dclrefbase_last_ref") dans la table wp_options de wordpress.
+     *
+     * Lors du premier appel, elle crée l'option et retourne la valeur 1. Lors
+     * des appels suivants, le compteur est incrémenté et la méthode retourne
+     * sa valeur actuelle.
+     *
+     * @param string $field Nom du champ pour lequel il faut générer une
+     * séquence.
+     *
+     * @return int
+     */
+    protected function sequenceIncrement($field) {
+        global $wpdb;
+
+        // Nom de l'option dans la table wp_options
+        $name = $this->sequenceName($field);
+
+        // Requête SQL à exécuter. Adapté de :
+        // @see http://answers.oreilly.com/topic/172-how-to-use-sequence-generators-as-counters-in-mysql/
+        $sql = "INSERT INTO `$wpdb->options` (`option_name`, `option_value`, `autoload`) "
+             . "VALUES('$name', 1, 'no') "
+             . "ON DUPLICATE KEY UPDATE `option_value` = LAST_INSERT_ID(`option_value` + 1)";
+
+        // Exécute la requête (pas de prepare car on contrôle les paramètres)
+        $affectedRows = $wpdb->query($sql);
+
+        return $affectedRows === 1 ? 1 : $wpdb->insert_id;
+
+        // Explications sur la requête SQL :
+        // @see http://dev.mysql.com/doc/refman/5.0/en/insert-on-duplicate.html
+        //
+        // With ON DUPLICATE KEY UPDATE, the affected-rows value per row is
+        // - 1 if the row is inserted as a new row
+        // - 2 if an existing row is updated
+        // - 0 if an existing row is set to its current values (i.e. n'a pas
+        //   été modifiée. Attention, dépend du flag CLIENT_FOUND_ROWS)
+        //
+        // If a table contains an AUTO_INCREMENT column and INSERT ... UPDATE
+        // inserts a row, the LAST_INSERT_ID() function returns the
+        // AUTO_INCREMENT value (i.e. l'ID de l'enreg créé).
+        // If the statement updates a row instead, LAST_INSERT_ID() is not
+        // meaningful. However, you can work around this by using
+        // LAST_INSERT_ID(expr) : c'est ce qu'on fait sur la dernière ligne,
+        // on "initialise" la valeur qui sera retournée par LAST_INSERT_ID().
+        //
+        // Dans notre cas :
+        // - premier appel, la ligne est insérée, insert retourne "affected-rows=1"
+        //   donc on sait que le compteur est à 1
+        // - appel suivant, la ligne est updatée, insert retourne "affected-rows=2"
+        //   il faut appeller LAST_INSERT_ID() pour obtenir la valeur du compteur.
+    }
+
+    /**
+     * Retourne le nom de la séquence pour le champ indiqué.
+     *
+     * Le nom de séquence correspond au nom de l'option qui sera créée dans la
+     * table wp_options de wordpress si cette séquence est utilisée.
+     *
+     * @param string $field
+     */
+    protected function sequenceName($field) {
+        return $this->postType . '_last_' . $field;
+    }
+
+    /**
+     * Réinitialise (supprime) une séquence, ou toutes les séquences d'une base
+     * si aucun champ n'est passé en paramètre.
+     *
+     * @param string $field Nom du champ ou vide pour supprimer toutes les
+     * séquences.
+     *
+     * @return int Le nombre de séquences supprimées.
+     */
+    protected function sequenceClear($field = null) {
+        global $wpdb;
+
+        if ($field) {
+            $op = '=';
+            $value = $this->sequenceName($field);
+        } else {
+            $op = ' LIKE ';
+            $value = $this->sequenceName('') . '%';
+        }
+
+        $sql = "DELETE FROM `$wpdb->options` WHERE `option_name` $op '$value'";
+
+        return $wpdb->query($sql);
+    }
+
+    /**
+     * Stocke la valeur passée en paramêtre dans la séquence du champ indiqué
+     * si celle-ci est supérieure à la valeur actuelle de la séquence.
+     *
+     * Cette méthode permette de mettre à jour une séquence quand le numéro est
+     * fournit par l'extérieur.
+     *
+     * Exemple d'utilisation :
+     *
+     * <code>
+     *     if (empty($ref))) $ref = sequenceIncrement('ref');
+     *     else sequenceSetIfGreater($ref);
+     * </code>
+     *
+     * @param string $field Nom du champ
+     * @param int $value Valeur à tester
+     *
+     * @return int Un code indiquant l'opération réalisée :
+     * - 0 : la séquence n'a pas été modifiée (sa valeur actuelle est supérieure
+     *   ou égale à $value).
+     *
+     * - 1 : la ligne a été créée (la séquence n'existait pas encore, elle a
+     *   été créée et initialisée avec $value).
+     *
+     * - 2 : la ligne a été mise à jour (la séquence existait mais sa valeur
+     *   était inférieure à $value, elle a été initialisée avec $value).
+     */
+    protected function sequenceSetIfGreater($field, $value) {
+        global $wpdb;
+
+        // Nom de l'option dans la table wp_options
+        $name = $this->sequenceName($field);
+
+        // Value doit être un entier
+        $value = (int) $value;
+
+        // Requête SQL à exécuter. Adapté de :
+        // @see http://stackoverflow.com/a/10081527
+        $sql = "INSERT INTO `$wpdb->options` (`option_name`, `option_value`, `autoload`) "
+             . "VALUES('$name', $value, 'no') "
+             . "ON DUPLICATE KEY UPDATE `option_value` = GREATEST(`option_value`, VALUES(`option_value`))";
+
+        // Exécute la requête (pas de prepare car on contrôle les paramètres)
+        return $wpdb->query($sql);
     }
 }

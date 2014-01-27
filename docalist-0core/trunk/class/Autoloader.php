9f9ac6612a091f6e802cc244ce28c374359fe4f5
Daniel Ménard <daniel-menard@users.noreply.github.com>
2014-01-27 12:39:26 +0000

    L'autoloader n'est plus une classe statique.

--- docalist-0core/trunk/class/Autoloader.php
+++ docalist-0core/trunk/class/Autoloader.php
@@ -2,7 +2,7 @@
 /**
  * This file is part of the "Docalist Core" plugin.
  *
- * Copyright (C) 2012, 2013 Daniel Ménard
+ * Copyright (C) 2012-2014 Daniel Ménard
  *
  * For copyright and license information, please view the
  * LICENSE.txt file that was distributed with this source code.
@@ -12,8 +12,8 @@
  * @author      Daniel Ménard <daniel.menard@laposte.net>
  * @version     SVN: $Id$
  */
-
 namespace Docalist;
+
 use Exception;
 
 /**
@@ -21,100 +21,65 @@ use Exception;
  */
 class Autoloader {
     /**
-     * @var array Liste des namespaces enregistrés par register().
+     * @var array Liste des espaces de noms enregistrés.
      *
-     * Les clés du tableau contiennent le namespace, les valeurs contiennent
-     * le path à utiliser pour charger les classes de ce namespace.
+     * Les clés du tableau contiennent l'espace de nom, les valeurs contiennent
+     * le path du répertoire qui contient les classes php de cet espace de noms.
      */
-    private static $path;
+    protected $path = array();
+
+    /**
+     * Crée un nouvel autoloader en enregistrant les espaces de noms passés en
+     * paramètre.
+     *
+     * @param array $namespaces un tableau de namespaces à enregistrer, de la
+     * forme :
+     * namespace => path du répertoire contenant les classes de ce namespace.
+     */
+    public function __construct(array $namespaces = array()) {
+        $this->path = $namespaces;
+        spl_autoload_register(array($this, 'autoload'), true);
+    }
 
     /**
      * Enregistre un espace de noms dans l'autoloader.
      *
-     * @param string $namespace Namespace à enregistrer (sensible à la casse).
+     * @param string $namespace Namespace à enregistrer
+     * (remarque : ne pas mettre d'antislash ni au début, ni à la fin).
      *
      * @param string $path Chemin absolu du dossier qui contient les classes
      * pour le namespace indiqué.
      */
-    public static function register($namespace, $path) {
-        // Premier appel
-        if (is_null(self::$path)) {
-            self::$path = array();
-
-            // @formatter:off
-            spl_autoload_register(array(__CLASS__, 'autoload'), true);
-            // @formatter:on
-        }
-
+    public function add($namespace, $path) {
         // Vérifie que ce namespace n'a pas déjà été enregistré
-        if (isset(self::$path[$namespace])) {
+        if (isset($this->path[$namespace])) {
             $msg = __('Le namespace %s est déjà enregistré (%s).', 'docalist-core');
-            throw new Exception(sprintf($msg, $namespace, self::$path[$namespace]));
+            throw new Exception(sprintf($msg, $namespace, $this->path[$namespace]));
         }
 
-        // Enregistre le namespace
-        $path = strtr($path, '/', DIRECTORY_SEPARATOR);
-        self::$path = array($namespace => $path) + self::$path;
-
+        // Enregistre le path
+        $this->path[$namespace] = $path;
     }
 
     /**
      * Autoloader. Cette fonction est appellée automatiquement par
      * spl_autoload_call lorsqu'une classe demandée n'existe pas.
      *
-     * Notre auloader ne sait charger que les classes dont le namespace
-     * a été enregistré dans {@link registerNamespace()}.
-     *
-     * En mode WP_DEBUG, des tests supplémentaires sont effectués et une
-     * aide est affichée si la classe demandée ne figure pas là où elle
-     * devrait.
-     *
      * @param string $class Nom complet de la classe à charger.
      */
-    private static function autoload($class) {
-        // Cet autoloader ne sait pas charger des classes sans namespace
-        if (false === strpos($class, '\\')) {
-            return;
-        }
+    protected function autoload($class) {
+        $namespace = $class;
+        while (false !== $pt = strrpos($namespace, '\\')) {
+            $namespace = substr($class, 0, $pt);
+            if (isset($this->path[$namespace])) {
+                $file = substr($class, $pt);
+                $file = strtr($file, '\\', DIRECTORY_SEPARATOR);
+                $file .= '.php';
+                $path = $this->path[$namespace] . $file;
 
-        // Regarde si la classe figure dans un namespace qu'on connait
-        foreach (self::$path as $namespace => $path) {
-            // Teste si les namespaces correspondent
-            if (strncmp($namespace, $class, strlen($namespace)) !== 0) {
-                continue;
-            }
-
-            // Détermine le path du fichier
-            $file = substr($class, strlen($namespace));
-            $file = strtr($file, '\\', DIRECTORY_SEPARATOR);
-            $path = $path . $file . '.php';
-
-            // En mode Debug : on fait des vérifs supplémentaires
-            if (WP_DEBUG) {
-                // Vérifie que le fichier existe
-                if (!is_file($path)) {
-                    $msg = __('Erreur dans %s : fichier non trouvé %s (classe %s)', 'docalist-core');
-                    throw new Exception(sprintf($msg, Utils::caller(), $path, $class));
-                }
-
-                // Charge le fichier
                 require_once $path;
-
-                // Vérifie que désormais la classe existe
-                if (!class_exists($class, false) && ! interface_exists($class, false) && ! trait_exists($class)) {
-                    $msg = __('Erreur dans %s : classe %s inexistante', 'docalist-core');
-                    throw new Exception(sprintf($msg, $path, $class));
-                }
-
-                // Ok
                 return;
             }
-
-            // Chargement en mode normal
-            require_once $path;
-
-            return;
         }
     }
-
 }

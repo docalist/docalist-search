181fce45b83777a6eb089c7a6e66259da182f0b1
Daniel Ménard <daniel-menard@users.noreply.github.com>
2014-02-05 10:57:00 +0000

    tableLookup.widget : ajout de la classe "do-not-clone" au script généré par le widget pour ce que celui-ci ne soit pas cloné lorsque le champ est dupliqué.

--- docalist-forms/trunk/src/themes/base/tablelookup.widget.php
+++ docalist-forms/trunk/src/themes/base/tablelookup.widget.php
@@ -10,6 +10,7 @@ $this->parentBlock($args);
 
 $writer->startElement('script');
 $writer->writeAttribute('type', 'text/javascript'); // pas nécessaire en html5
+$writer->writeAttribute('class', 'do-not-clone'); // indique à deocalist-forms.js qu'il ne faut pas cloner cet élément
 
 $id = $this->attribute('id');
 $writer->writeRaw("jQuery('#$id').tableLookup();");

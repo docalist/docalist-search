86b6678cbadef5e9c4dfed19c3f5356bfcc1efc3
Daniel Ménard <daniel-menard@users.noreply.github.com>
2014-08-22 13:54:12 +0000

    IndexerSettings : adaptation aux nouveaux types + formattage, etc.

--- class/IndexerSettings.php
+++ class/IndexerSettings.php
@@ -2,7 +2,7 @@
 /**
  * This file is part of the "Docalist Search" plugin.
  *
- * Copyright (C) 2012, 2013 Daniel Ménard
+ * Copyright (C) 2012-2014 Daniel Ménard
  *
  * For copyright and license information, please view the
  * LICENSE.txt file that was distributed with this source code.
@@ -14,37 +14,37 @@
  */
 namespace Docalist\Search;
 
-use Docalist\Data\Entity\AbstractEntity;
+use Docalist\Type\Object;
+use Docalist\Type\Integer;
 
 /**
  * Paramètres de l'indexeur.
  *
- * @property string[] $types Contenus à indexer.
- * @property int $bulkMaxSize Taille maximale du buffer (Mo).
- * @property int $bulkMaxCount Nombre maximum de documents dans le buffer.
+ * @property String[] $types Contenus à indexer.
+ * @property Integer $bulkMaxSize Taille maximale du buffer (Mo).
+ * @property Integer $bulkMaxCount Nombre maximum de documents dans le buffer.
  */
-class IndexerSettings extends AbstractEntity {
-
-    protected function loadSchema() {
+class IndexerSettings extends Object {
+    static protected function loadSchema() {
         // @formatter:off
-        return array(
-            'types' => array(
+        return [
+            'types' => [
                 'label' => __('Contenus à indexer', 'docalist-search'),
                 'type' => 'string*',
-            ),
-            'bulkMaxSize' => array(
+            ],
+            'bulkMaxSize' => [
                 'label' =>__('Taille maximale du buffer (en Mo)', 'docalist-search'),
                 'description' => __('En méga-octets. Le buffer est vidé si la taille totale des documents en attente dépasse cette limite.', 'docalist-search'),
                 'type' => 'int',
                 'default' => 10,
-            ),
-            'bulkMaxCount' => array(
+            ],
+            'bulkMaxCount' => [
                 'label' =>__('Nombre maximum de documents', 'docalist-search'),
                 'description' => __('Le buffer est vidé si le nombre de documents en attente dépasse ce nombre.', 'docalist-search'),
                 'type' => 'int',
                 'default' => 10000,
-            ),
-        );
+            ],
+        ];
         // @formatter:off
     }
 }

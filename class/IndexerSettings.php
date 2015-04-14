f9c70acc91ec24b251d2a5db9f4591553db55cdc
Daniel Ménard <daniel-menard@users.noreply.github.com>
2015-04-14 08:25:03 +0000

    IndexerSetting s: améliore le libellé de bulkmaxsize.

--- class/IndexerSettings.php
+++ class/IndexerSettings.php
@@ -2,7 +2,7 @@
 /**
  * This file is part of the "Docalist Search" plugin.
  *
- * Copyright (C) 2012-2014 Daniel Ménard
+ * Copyright (C) 2012-2015 Daniel Ménard
  *
  * For copyright and license information, please view the
  * LICENSE.txt file that was distributed with this source code.
@@ -34,7 +34,7 @@ class IndexerSettings extends Object {
                     'type' => 'string*',
                 ],
                 'bulkMaxSize' => [
-                    'label' =>__('Taille maximale du buffer (en Mo)', 'docalist-search'),
+                    'label' =>__('Taille maximale du buffer', 'docalist-search'),
                     'description' => __('En méga-octets. Le buffer est vidé si la taille totale des documents en attente dépasse cette limite.', 'docalist-search'),
                     'type' => 'int',
                     'default' => 10,

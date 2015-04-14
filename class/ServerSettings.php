9154ef07957170bbb04c7e2901d8ff9eaebcaae2
Daniel Ménard <daniel-menard@users.noreply.github.com>
2015-04-14 08:21:10 +0000

    ServerSettings : options pour le nouveau client ES
    - ajout de la propriété connecttimeout
    - ajout de la propriété compressrequest
    - ajout de la propriété compressresponse

--- class/ServerSettings.php
+++ class/ServerSettings.php
@@ -2,7 +2,7 @@
 /**
  * This file is part of the "Docalist Search" plugin.
  *
- * Copyright (C) 2012-2014 Daniel Ménard
+ * Copyright (C) 2012-2015 Daniel Ménard
  *
  * For copyright and license information, please view the
  * LICENSE.txt file that was distributed with this source code.
@@ -23,7 +23,10 @@ use Docalist\Type\Integer;
  *
  * @property String $url Url complète du serveur ElasticSearch.
  * @property String $index Nom de l'index ElasticSearch utilisé.
+ * @property Integer $connecttimeout Timeout de connexion, en secondes.
  * @property Integer $timeout Timeout des requêtes, en secondes.
+ * @property Boolean $compressrequest Compresser les requêtes.
+ * @property Boolean $compressresponse Compresser les réponses.
  */
 class ServerSettings extends Object {
     // @formatter:off
@@ -34,8 +37,8 @@ class ServerSettings extends Object {
             'fields' => [
                 'url' => [
                     'label' =>__('Url de ElasticSearch', 'docalist-search'),
-                    'description' => __("Url complète du moteur ElasticSearch à utiliser (par exemple : <code>http://127.0.0.1:9200/</code>).", 'docalist-search'),
-                    'default' => 'http://127.0.0.1:9200/',
+                    'description' => __("Url complète du moteur ElasticSearch à utiliser (par exemple : <code>http://127.0.0.1:9200</code>).", 'docalist-search'),
+                    'default' => 'http://127.0.0.1:9200',
                 ],
                 'index' => [
                     'label' =>__("Nom de l'index à utiliser", 'docalist-search'),
@@ -44,11 +47,29 @@ class ServerSettings extends Object {
                     // Evite que deux sites sur le même serveur partagent par erreur le même index
                     'default' => $wpdb->get_blog_prefix() . DB_NAME,
                 ],
+                'connecttimeout' => [
+                    'label' =>__('Timeout de connexion', 'docalist-search'),
+                    'description' => __("Si la connexion avec le serveur ElasticSearch n'est pas établie au bout du nombre de secondes indiqué, une erreur sera générée.", 'docalist-search'),
+                    'type' => 'int',
+                    'default' => 1,
+                ],
                 'timeout' => [
-                    'label' =>__('Timeout des requêtes, en secondes', 'docalist-search'),
-                    'description' => __("Si le serveur ElasticSearch n'a pas répondu au bout du délai imparti, une erreur sera générée.", 'docalist-search'),
+                    'label' =>__('Timeout des requêtes', 'docalist-search'),
+                    'description' => __("Si le serveur ElasticSearch n'a pas répondu au bout du nombre de secondes indiqué, une erreur sera générée.", 'docalist-search'),
                     'type' => 'int',
                     'default' => 10,
+                ],
+                'compressrequest' => [
+                    'label' =>__('Compresser les requêtes', 'docalist-search'),
+                    'description' => __("Compresse les requêtes envoyées au serveur. N'activez cette option que si votre serveur ElasticSearch sait décoder les requêtes compressées.", 'docalist-search'),
+                    'type' => 'bool',
+                    'default' => false,
+                ],
+                'compressresponse' => [
+                    'label' =>__('Compresser les réponses', 'docalist-search'),
+                    'description' => __("Demande à ElasticSearch de compresser les réponses retournées. Cette option n'a aucun effet si l'option <a href='http://www.elastic.co/guide/en/elasticsearch/reference/current/modules-http.html'>http.compression</a> n'est pas activée sur le serveur ElasticSearch.", 'docalist-search'),
+                    'type' => 'bool',
+                    'default' => false,
                 ]
             ]
         ];

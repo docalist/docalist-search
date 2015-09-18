1dc723276219062cbc3088647d1c97413b6f0ead
Daniel Ménard <daniel.menard@laposte.net>
2015-09-18 18:33:24 +0200

    ServerSettings : Ajuste les types indiqués dans le schéma
    - Object s'appele maintenant Composite, String s'appelle maintenant Text
    - utilise les types exact plutôt que les alias 'int' et 'bool'.
    - Ajout de use manquants (pour les entrées @property de la phpdoc)

--- class/ServerSettings.php
+++ class/ServerSettings.php
@@ -13,21 +13,22 @@
  */
 namespace Docalist\Search;
 
-use Docalist\Type\Object;
-use Docalist\Type\String;
+use Docalist\Type\Composite;
+use Docalist\Type\Text;
 use Docalist\Type\Integer;
+use Docalist\Type\Boolean;
 
 /**
  * Options de configuration du serveur ElasticSearch.
  *
- * @property String $url Url complète du serveur ElasticSearch.
- * @property String $index Nom de l'index ElasticSearch utilisé.
+ * @property Text $url Url complète du serveur ElasticSearch.
+ * @property Text $index Nom de l'index ElasticSearch utilisé.
  * @property Integer $connecttimeout Timeout de connexion, en secondes.
  * @property Integer $timeout Timeout des requêtes, en secondes.
  * @property Boolean $compressrequest Compresser les requêtes.
  * @property Boolean $compressresponse Compresser les réponses.
  */
-class ServerSettings extends Object {
+class ServerSettings extends Composite {
     // @formatter:off
     static protected function loadSchema() {
         global $wpdb;
@@ -49,25 +50,25 @@ class ServerSettings extends Object {
                 'connecttimeout' => [
                     'label' =>__('Timeout de connexion', 'docalist-search'),
                     'description' => __("Si la connexion avec le serveur ElasticSearch n'est pas établie au bout du nombre de secondes indiqué, une erreur sera générée.", 'docalist-search'),
-                    'type' => 'int',
+                    'type' => 'Docalist\Type\Integer',
                     'default' => 1,
                 ],
                 'timeout' => [
                     'label' =>__('Timeout des requêtes', 'docalist-search'),
                     'description' => __("Si le serveur ElasticSearch n'a pas répondu au bout du nombre de secondes indiqué, une erreur sera générée.", 'docalist-search'),
-                    'type' => 'int',
+                    'type' => 'Docalist\Type\Integer',
                     'default' => 10,
                 ],
                 'compressrequest' => [
                     'label' =>__('Compresser les requêtes', 'docalist-search'),
                     'description' => __("Compresse les requêtes envoyées au serveur. N'activez cette option que si votre serveur ElasticSearch sait décoder les requêtes compressées.", 'docalist-search'),
-                    'type' => 'bool',
+                    'type' => 'Docalist\Type\Boolean',
                     'default' => false,
                 ],
                 'compressresponse' => [
                     'label' =>__('Compresser les réponses', 'docalist-search'),
                     'description' => __("Demande à ElasticSearch de compresser les réponses retournées. Cette option n'a aucun effet si l'option <a href='http://www.elastic.co/guide/en/elasticsearch/reference/current/modules-http.html'>http.compression</a> n'est pas activée sur le serveur ElasticSearch.", 'docalist-search'),
-                    'type' => 'bool',
+                    'type' => 'Docalist\Type\Boolean',
                     'default' => false,
                 ]
             ]

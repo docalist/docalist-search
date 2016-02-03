e3c05fdce5969d29f20a1ecaabecd7f99d4ad4a7
Daniel Ménard <daniel.menard@laposte.net>
2016-02-03 16:00:52 +0100

    ServerSettings : Revoir la description des options

--- class/ServerSettings.php
+++ class/ServerSettings.php
@@ -38,14 +38,27 @@ class ServerSettings extends Composite
             'fields' => [
                 'url' => [
                     'type' => 'Docalist\Type\Text',
-                    'label' => __('Url de ElasticSearch', 'docalist-search'),
-                    'description' => __('Url complète du moteur ElasticSearch à utiliser (par exemple : <code>http://127.0.0.1:9200</code>).', 'docalist-search'),
+                    'label' => __('Url du cluster ElasticSearch', 'docalist-search'),
+                    'description' => __(
+                        "Url complète de votre cluster ElasticSearch (par exemple : <code>http://127.0.0.1:9200</code>).
+                         Vous devez indiquer le protocole utilisé (http ou https), l'adresse IP ou le nom DNS du
+                         cluster et le port TCP à utiliser s'il est spécifique.
+                         Si votre cluster est protégé par un login et un mot de passe, indiquez-les en utilisant la
+                        syntaxe suivante : <code>https://user:password@search.example.org:9200</code>.
+                        ",
+                        'docalist-search'
+                    ),
                     'default' => 'http://127.0.0.1:9200',
                 ],
                 'index' => [
                     'type' => 'Docalist\Type\Text',
-                    'label' => __("Nom de l'index à utiliser", 'docalist-search'),
-                    'description' => __("Nom de l'index ElasticSearch qui contiendra tous les contenus indexés. <b>Attention</b> : vérifiez que cet index n'existe pas déjà.", 'docalist-search'),
+                    'label' => __("Nom de base de l'index", 'docalist-search'),
+                    'description' => __(
+                        "Préfixe qui sera utilisé pour déterminer le nom des index et des alias ElasticSearch
+                         qui contiendront les contenus indexés. Assurez-vous que le préfixe indiqué soit unique :
+                         les index ne doivent pas être partagés entre plusieurs sites docalist.",
+                        'docalist-search'
+                    ),
                     // Par défaut : préfixe des tables + nom de la base (ex wp_prisme)
                     // Evite que deux sites sur le même serveur partagent par erreur le même index
                     'default' => $wpdb->get_blog_prefix() . DB_NAME,
@@ -53,25 +66,42 @@ class ServerSettings extends Composite
                 'connecttimeout' => [
                     'type' => 'Docalist\Type\Integer',
                     'label' => __('Timeout de connexion', 'docalist-search'),
-                    'description' => __("Si la connexion avec le serveur ElasticSearch n'est pas établie au bout du nombre de secondes indiqué, une erreur sera générée.", 'docalist-search'),
+                    'description' => __(
+                        "En secondes. Si la connexion avec le cluster ElasticSearch n'est pas établie au bout
+                         du nombre de secondes indiqué, une erreur sera générée.",
+                        'docalist-search'
+                    ),
                     'default' => 1,
                 ],
                 'timeout' => [
                     'type' => 'Docalist\Type\Integer',
                     'label' => __('Timeout des requêtes', 'docalist-search'),
-                    'description' => __("Si le serveur ElasticSearch n'a pas répondu au bout du nombre de secondes indiqué, une erreur sera générée.", 'docalist-search'),
+                    'description' => __(
+                        "En secondes. Si le serveur ElasticSearch n'a pas répondu au bout du nombre
+                         de secondes indiqué, une erreur sera générée.",
+                        'docalist-search'
+                    ),
                     'default' => 10,
                 ],
                 'compressrequest' => [
                     'type' => 'Docalist\Type\Boolean',
                     'label' => __('Compresser les requêtes', 'docalist-search'),
-                    'description' => __("Compresse les requêtes envoyées au serveur. N'activez cette option que si votre serveur ElasticSearch sait décoder les requêtes compressées.", 'docalist-search'),
+                    'description' => __(
+                        "Compresse les requêtes envoyées au serveur. N'activez cette option que si votre serveur
+                         ElasticSearch sait décoder les requêtes compressées.",
+                        'docalist-search'
+                    ),
                     'default' => false,
                 ],
                 'compressresponse' => [
                     'type' => 'Docalist\Type\Boolean',
                     'label' => __('Compresser les réponses', 'docalist-search'),
-                    'description' => __("Demande à ElasticSearch de compresser les réponses retournées. Cette option n'a aucun effet si l'option <a href='http://www.elastic.co/guide/en/elasticsearch/reference/current/modules-http.html'>http.compression</a> n'est pas activée sur le serveur ElasticSearch.", 'docalist-search'),
+                    'description' => sprintf(__(
+                        "Demande à ElasticSearch de compresser les réponses retournées. Cette option n'a aucun effet
+                         si l'option <a href='%s'>http.compression</a> n'est pas activée sur le serveur ElasticSearch.",
+                        'docalist-search'),
+                        'http://www.elastic.co/guide/en/elasticsearch/reference/current/modules-http.html'
+                    ),
                     'default' => false,
                 ],
             ],

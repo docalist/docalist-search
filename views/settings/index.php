bc8632d273d414cb575a1742e2b1bcdeecacfb01
Daniel Ménard <daniel.menard@laposte.net>
2016-02-13 18:27:36 +0100

    view/index : adaptation

--- views/settings/index.php
+++ views/settings/index.php
@@ -22,36 +22,37 @@ use Docalist\Search\SettingsPage;
  */
 ?>
 <div class="wrap">
-    <h1><?= __("Paramètres de Docalist Search", 'docalist-search') ?></h1>
+    <h1><?= __("Réglages de Docalist Search", 'docalist-search') ?></h1>
 
     <p class="description"><?php
-        //@formatter:off
         echo __(
-            'Docalist Search est un plugin qui permet de doter votre site
-            WordPress d\'un moteur de recherche moderne et performant.
-            Utilisez les liens ci-dessous pour paramétrer votre moteur.',
+            "Docalist Search est un plugin qui permet de doter votre site WordPress d'un moteur de recherche moderne
+             et performant. Utilisez les liens ci-dessous pour paramétrer votre moteur.",
             'docalist-search'
         );
-        // @formatter:on
     ?></p>
 
     <h2>
         <a href="<?= esc_url($this->url('ServerSettings')) ?>">
-            <?= __('Paramètres ElasticSearch', 'docalist-search') ?>
+            <?= __('Paramètres Docalist-Search', 'docalist-search') ?>
         </a>
     </h2>
     <p class="description">
-        <?= __('Serveur et index ElasticSearch à utiliser, timeout des requêtes.', 'docalist-search') ?>
+        <?= __(
+                "Serveur et index ElasticSearch à utiliser, timeout des requêtes et paramètres d'indexation.",
+                'docalist-search'
+            )
+        ?>
     </p>
 
 
     <h2>
-        <a href="<?= esc_url($this->url('IndexerSettings')) ?>">
-            <?= __("Paramètres de l'indexeur", 'docalist-search') ?>
+        <a href="<?= esc_url($this->url('CreateIndex')) ?>">
+            <?= __("Créer l'index de recherche", 'docalist-search') ?>
         </a>
     </h2>
     <p class="description">
-        <?= __("Contenus à indexer et options d'indexation.", 'docalist-search') ?>
+        <?= __('Choix des contenus à indexer et indexation initiale des contenus existants.', 'docalist-search') ?>
     </p>
 
 
@@ -63,17 +64,6 @@ use Docalist\Search\SettingsPage;
     <p class="description">
         <?= __("Choix de la page des réponses, activation de la recherche Docalist Search.", 'docalist-search') ?>
     </p>
-
-
-    <h2>
-        <a href="<?= esc_url($this->url('Reindex')) ?>">
-            <?= __("Réindexation manuelle", 'docalist-search') ?>
-        </a>
-    </h2>
-    <p class="description">
-        <?= __("Permet de lancer une réindexation complète des contenus indexés.", 'docalist-search') ?>
-    </p>
-
 <?php /*
     <h2>
         <a href="<?= esc_url($this->url('ServerStatus')) ?>">

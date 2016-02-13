ae0fbed85008a647b7946f0fd3454c1be8f20f34
Daniel Ménard <daniel.menard@laposte.net>
2016-02-13 12:12:33 +0100

    PostIndexer : adaptation / réécriture
    - implémente la nouvelle méthode buildIndexSettings() qui remplace
    getMapping()
    - renomme realtime() en activeRealtime()
    - évite d'utiler le global $wpdb
    - gestion des champs WP_Post standard (dans TypeIndexer avant)

--- class/PostIndexer.php
+++ class/PostIndexer.php
@@ -13,8 +13,10 @@
  */
 namespace Docalist\Search;
 
+use wpdb;
 use WP_Post;
 use Docalist\MappingBuilder;
+use InvalidArgumentException;
 
 /**
  * Un indexeur pour les articles de WordPress.
@@ -22,11 +24,15 @@ use Docalist\MappingBuilder;
 class PostIndexer extends TypeIndexer
 {
     /**
-     * Construit l'indexeur.
+     * Liste des champs WordPress standard qu'on sait indexer.
      *
-     * @param string $type Optionnel, le type de contenu géré par cet indexeur ("post" par défaut, mais les classes
-     * descendantes peuvent passer un autre type).
+     * @var string[]
      */
+    protected static $stdFields = [
+        'ID', 'post_type', 'post_status', 'post_name', 'post_parent', 'post_author', 'post_date', 'post_modified',
+        'post_title', 'post_content',  'post_excerpt',
+    ];
+
     public function __construct($type = 'post')
     {
         parent::__construct($type);
@@ -39,25 +45,10 @@ class PostIndexer extends TypeIndexer
 
     public function getCategory()
     {
-        return __('WordPress', 'docalist-search');
+        return __('Contenus WordPress', 'docalist-search');
     }
 
-    /**
-     * Retourne la liste des status à indexer.
-     *
-     * @return string
-     */
-    public function getStatuses()
-    {
-        return ['publish', 'pending', 'private'];
-    }
-
-    public function getID($post) /* @var $post WP_Post */
-    {
-        return $post->ID;
-    }
-
-    public function getMapping()
+    public function buildIndexSettings(array $settings)
     {
         $mapping = docalist('mapping-builder'); /* @var MappingBuilder $mapping */
         $mapping->reset()->setDefaultAnalyzer('fr-text'); // todo : rendre configurable
@@ -66,50 +57,38 @@ class PostIndexer extends TypeIndexer
             static::standardMapping($field, $mapping);
         }
 
-        return $mapping->getMapping();
+        $settings['mappings'][$this->getType()] = $mapping->getMapping();
+
+        return $settings;
     }
 
-
-    public function map($post) /* @var $post WP_Post */
+    public function activeRealtime()
     {
-        $document = [];
-        foreach (self::$stdFields as $field) {
-            $value = $post->$field;
-            $value && static::standardMap($field, $value, $document);
-        }
+        add_action('transition_post_status', function ($newStatus, $oldStatus, WP_Post $post) {
+            $this->onStatusChange($newStatus, $oldStatus, $post);
+        }, 10, 3);
 
-        return $document;
+            add_action('delete_post', function ($id) {
+                $this->onDelete($id);
+            });
     }
 
-    /**
-     * Réindexe tous les documents de ce type.
-     *
-     * @param Indexer $indexer
-     */
     public function indexAll(Indexer $indexer)
     {
-        global $wpdb;
-
+        $wpdb = docalist('wordpress-database'); /* @var wpdb $wpdb */
         $offset = 0;
         $limit = 1000;
 
         // Prépare la requête utilisée pour charger les posts par lots de $limit
         $sql = sprintf(
-           'SELECT * FROM %s '
-         . "WHERE post_type = '%s' AND post_status IN ('%s') "
-         . 'ORDER BY ID ASC '
-         . 'LIMIT %s OFFSET %s',
-
+            "SELECT * FROM %s WHERE post_type='%s' AND post_status IN ('%s') ORDER BY ID ASC LIMIT %%d OFFSET %%d",
             $wpdb->posts,
             $this->getType(),
-            implode("','", $this->getStatuses()),
-            '%d',
-            '%d'
+            implode("','", $this->getStatuses())
         );
 
-        // remarque : pas besoin d'appeler prepare(). Un post_type ou un
-        // statut ne contiennent que des lettres et on contrôle les deux autres
-        // entiers passés en paramètre.
+        // remarque : pas besoin d'appeler prepare(). Un post_type ou un statut ne contiennent que des lettres et
+        // on contrôle les deux autres entiers passés en paramètre.
 
         for (;;) {
             // Prépare la requête pour le prochain lot
@@ -130,21 +109,34 @@ class PostIndexer extends TypeIndexer
 
             // Passe au lot suivant
             $offset += count($posts);
-
-            // La ligne (commentée) suivante est pratique pour les tests
-            // if ($offset >= 1000) break;
         }
     }
 
-    public function realtime()
+    protected function getID($post) /* @var $post WP_Post */
     {
-        add_action('transition_post_status', function ($newStatus, $oldStatus, WP_Post $post) {
-            $this->onStatusChange($newStatus, $oldStatus, $post);
-        }, 10, 3);
+        return $post->ID;
+    }
 
-        add_action('delete_post', function ($id) {
-            $this->onDelete($id);
-        });
+    protected function map($post) /* @var $post WP_Post */
+    {
+        $document = [];
+        foreach (self::$stdFields as $field) {
+            $value = $post->$field;
+            $value && static::standardMap($field, $value, $document);
+        }
+
+        return $document;
+    }
+
+
+    /**
+     * Retourne la liste des status à indexer.
+     *
+     * @return string
+     */
+    protected function getStatuses()
+    {
+        return ['publish', 'pending', 'private'];
     }
 
     /**
@@ -192,7 +184,7 @@ class PostIndexer extends TypeIndexer
      *
      * @param int $id
      */
-    public function onDelete($id)
+    protected function onDelete($id)
     {
         $post = get_post($id);
 
@@ -208,5 +200,107 @@ class PostIndexer extends TypeIndexer
         /* @var $indexer Indexer */
         $indexer = docalist('docalist-search-indexer');
         $indexer->delete($this->type, $post->ID);
+    }
+
+    /**
+     * Génère le mapping standard à utiliser pour un champ WordPress.
+     *
+     * @param string $field Le nom d'un champ WP_Post.
+     * @param MappingBuilder $mapping Le mapping à modifier.
+     *
+     * @throws InvalidArgumentException Si le champ indiqué n'est pas géré.
+     */
+    public static function standardMapping($field, MappingBuilder $mapping)
+    {
+        switch ($field) {
+            case 'ID':              // non indexé, on a déjà _id géré par ES
+            case 'post_type':       // non indexé, on a déjà _type géré par ES
+                return;
+            case 'post_status':     return $mapping->addField('status')->text()->filter();
+            case 'post_name':       return $mapping->addField('slug')->text();
+            case 'post_parent':     return $mapping->addField('parent')->integer();
+            case 'post_author':     return $mapping->addField('createdby')->text()->filter();
+            case 'post_date':       return $mapping->addField('creation')->dateTime();
+            case 'post_modified':   return $mapping->addField('lastupdate')->dateTime();
+            case 'post_title':      return $mapping->addField('title')->text();
+            case 'post_content':    return $mapping->addField('content')->text();
+            case 'post_excerpt':    return $mapping->addField('excerpt')->text();
+            default:
+                throw new InvalidArgumentException("Field '$field' not supported");
+        }
+    }
+
+    /**
+     * Mappe un champ WordPress standard.
+     *
+     * @param string $field Le nom du champ
+     * @param la valeur du champ $value
+     * @param array $document Le document à génerer.
+     *
+     * @throws InvalidArgumentException Si le champ indiqué n'est pas géré.
+     */
+    public static function standardMap($field, $value, array & $document)
+    {
+        switch ($field) {
+            case 'ID':
+                return; // non indexé, on a déjà _id géré par ES
+
+            case 'post_type':
+                return; // non indexé, on a déjà _type géré par ES
+
+            case 'post_status':
+                if (! is_null($status = get_post_status_object($value))) {
+                    $value = $status->label;
+                }
+                $document['status'] = $value;
+
+                return;
+
+            case 'post_name':
+                $document['slug'] = $value;
+
+                return;
+
+            case 'post_parent':
+                $document['parent'] = (int) $value;
+
+                return;
+
+            case 'post_author':
+                if (false !== $user = get_user_by('id', $value)) { /* @var $user WP_User */
+                    $value = $user->user_login;
+                }
+                $document['createdby'] = $value;
+
+                return;
+
+            case 'post_date':
+                $document['creation'] = $value;
+
+                return;
+
+            case 'post_modified':
+                $document['lastupdate'] = $value;
+
+                return;
+
+            case 'post_title':
+                $document['title'] = $value;
+
+                return;
+
+            case 'post_content':
+                $document['content'] = $value;
+
+                return;
+
+            case 'post_excerpt':
+                $document['excerpt'] = $value;
+
+                return;
+
+            default:
+                throw new InvalidArgumentException("Field '$field' not supported");
+        }
     }
 }

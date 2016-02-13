0e11997296c4eab0b76f9579a7d34a64c3b5c38f
Daniel Ménard <daniel.menard@laposte.net>
2016-02-13 20:24:22 +0100

    NullIndexer : correction de la phpdoc.

--- class/Indexer/NullIndexer.php
+++ class/Indexer/NullIndexer.php
@@ -19,14 +19,11 @@ use Docalist\Search\IndexManager;
 /**
  * Un indexeur qui ne fait rien.
  *
- * Cette classe sert à remplacer un indexeur manquant ou invalide (cf. la
- * méthode Indexer::indexer()).
+ * Cette classe sert à remplacer un indexeur manquant ou invalide (cf. la méthode IndexManager::getIndexer()).
  *
- * Quand on désactive un plugin, on peut se retrouver avec un contenu indexé
- * pour lequel l'indexeur n'est plus disponible (par exemple, on a indexé
- * une base documentaire puis on désactive docalist-biblio). Cette classe sert
- * à gérer ce cas : au lieu de générer une erreur fatale (c'est ce que ça
- * faisait avant), l'indexeur manquant est remplacé par un NullIndexer et une
+ * Quand on désactive un plugin, on peut se retrouver avec un contenu indexé pour lequel l'indexeur n'est plus
+ * disponible (par exemple, on a indexé une base documentaire puis on désactive docalist-biblio). Cette classe sert
+ * à gérer ce cas : au lieu de générer une erreur fatale, l'indexeur manquant est remplacé par un NullIndexer et une
  * admin notice est générée.
  */
 class NullIndexer extends AbstractIndexer

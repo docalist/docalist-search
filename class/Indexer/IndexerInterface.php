1f7c2580af18b631d2e9d91128bc8453c02ebee4
Daniel Ménard <daniel.menard@laposte.net>
2016-03-24 20:13:34 +0100

    IndexerInterface : ajout des méthodes getCollection et getSearchFilter

--- class/Indexer/IndexerInterface.php
+++ class/Indexer/IndexerInterface.php
@@ -21,11 +21,8 @@ use Docalist\Search\IndexManager;
  * Le rôle d'un indexeur consiste à transformer un contenu quelconque (article, page, notice, profil utilisateur,
  * commentaire, produit...) en document destiné à être indexé par ElasticSearch.
  *
- * Chaque indexeur gère un seul type de contenu et dispose de méthode permettant de convertir un objet en document
- * ElasticSearch, d'indexer / mettre à jour / supprimer un objet, de réindexer la totalité des objets de ce type.
- *
- * Cette classe de base contient également des méthodes qui permettent de mapper de façon homogène entre différents
- * types certains des champs standard de WordPress (post_status, post_title, etc.)
+ * Chaque indexeur gère un seul type de contenu et dispose de méthodes permettant de convertir ce contenu en document
+ * ElasticSearch, d'indexer / mettre à jour / supprimer un contenu, de réindexer la totalité des contenus de ce type.
  */
 interface IndexerInterface
 {
@@ -51,6 +48,13 @@ interface IndexerInterface
     public function getCategory();
 
     /**
+     * Retourne le code à utiliser pour désigner une collection de contenus de ce type.
+     *
+     * Utilisé pour initialiser le champ 'in' lors de l'indexation.
+     */
+    public function getCollection();
+
+    /**
      * Construit les settings de l'index ElasticSearch.
      *
      * Cette méthode permet à l'indexeur d'ajouter dans les settings de l'index les analyseurs et les mappings
@@ -74,9 +78,23 @@ interface IndexerInterface
     public function activateRealtime(IndexManager $indexManager);
 
     /**
-     * Indexe tous les documents gérés par l'indexeur.
+     * Indexe tous les contenus gérés par l'indexeur.
      *
      * @param IndexManager $indexManager Le gestionnaire d'index docalist-search.
      */
     public function indexAll(IndexManager $indexManager);
+
+    /**
+     * Retourne les filtres à appliquer pour une recherche portant sur les contenus gérés par cet indexeur.
+     *
+     * Cette méthode permet de filtrer les réponses obtenues pour une recherche en excluant les documents auxquels
+     * l'utilisateur WordPress en cours n'a pas accès.
+     *
+     * Le filtre généré doit contenir une clause permettant de sélectionner les contenus gérés par cet indexeur
+     * (par exemple "in:collection") mais peut aussi contenir des clauses portant sur le statut des documents
+     * (par exemple "status:public") ou d'autres critères.
+     *
+     * @return array Un tableau contenant un filtre de filtres elastic-search. (par exemple : status:public or user:me)
+     */
+    public function getSearchFilter();
 }

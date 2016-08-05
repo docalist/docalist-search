7ac3179a27f99f5ab27ca2b1a653b5caa27edb75
Daniel Ménard <daniel.menard@laposte.net>
2016-08-05 17:17:48 +0200

    Classe de base pour les agrégations de type "multi-metrics"

---
+++ class/Aggregation/MultiMetricsAggregation.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * This file is part of the "Docalist Search" plugin.
+ *
+ * Copyright (C) 2013-2016 Daniel Ménard
+ *
+ * For copyright and license information, please view the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * @package     Docalist
+ * @subpackage  Search
+ * @author      Daniel Ménard <daniel.menard@laposte.net>
+ */
+namespace Docalist\Search\Aggregation;
+
+/**
+ * Classe de base pour les agrégations de type "metrics" qui retournent plusieurs valeurs.
+ *
+ * @link https://www.elastic.co/guide/en/elasticsearch/reference/master/search-aggregations-metrics.html
+ */
+abstract class MultiMetricsAggregation extends MetricsAggregation
+{
+}

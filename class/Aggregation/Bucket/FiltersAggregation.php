3c848f4a230198d9bfaa41a63a21af813c04f669
Daniel Ménard <daniel.menard@laposte.net>
2016-08-05 17:29:54 +0200

    FiltersAggregation

---
+++ class/Aggregation/Bucket/FiltersAggregation.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * This file is part of the "Docalist Search" plugin.
+ *
+ * Copyright (C) 2013-2016 Daniel Ménard
+ *
+ * For copyright and license information, please view the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * @package     Docalist
+ * @subpackage  Search
+ * @author      Daniel Ménard <daniel.menard@laposte.net>
+ */
+namespace Docalist\Search\Aggregation\Bucket;
+
+use Docalist\Search\Aggregation\MultiBucketsAggregation;
+
+/**
+ * Une agrégation de type "buckets" qui regroupe les documents qui correspondent aux filtres indiqués.
+ *
+ * @link https://www.elastic.co/guide/en/elasticsearch/reference/master/search-aggregations-bucket-filters-aggregation.html
+ */
+class FiltersAggregation extends MultiBucketsAggregation
+{
+    const TYPE = 'filters';
+
+    /**
+     * Constructeur
+     *
+     * @param array $filters Une liste de filtres à appliquer à l'agrégation. Les clés du tableau indiquent le nom
+     * des filtres et les valeurs associées contiennent la définition DSL du filtre.
+     */
+    public function __construct(array $filters)
+    {
+        parent::__construct(['filters' => $filters]);
+    }
+}

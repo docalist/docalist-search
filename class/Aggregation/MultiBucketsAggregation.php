544b091d9bf2b2419eeedc2436564f7b74b8bc70
Daniel Ménard <daniel.menard@laposte.net>
2016-08-05 17:26:05 +0200

    Classe de base pour les agrégations de type "multi-buckets".

---
+++ class/Aggregation/MultiBucketsAggregation.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * This file is part of the "Docalist Search" plugin.
+ *
+ * Copyright (C) 2013-2016 Daniel Ménard
+ *
+ * For copyright and license information, please view the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * @package     Docalist
+ * @subpackage  Search
+ * @author      Daniel Ménard <daniel.menard@laposte.net>
+ */
+namespace Docalist\Search\Aggregation;
+
+/**
+ * Classe de base pour les agrégations de type "bucket" qui retournent plusieurs buckets.
+ *
+ * @link https://www.elastic.co/guide/en/elasticsearch/reference/master/search-aggregations-bucket.html
+ */
+abstract class MultiBucketsAggregation extends BucketAggregation
+{
+}

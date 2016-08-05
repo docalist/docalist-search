0ff4060ebdcba88956d161f84b93ef6b51167f6e
Daniel Ménard <daniel.menard@laposte.net>
2016-08-05 17:22:05 +0200

    CardinalityAggregation (nombre de valeurs distinctes)

---
+++ class/Aggregation/Metrics/CardinalityAggregation.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * This file is part of the "Docalist Search" plugin.
+ *
+ * Copyright (C) 2013-2016 Daniel Ménard
+ *
+ * For copyright and license information, please view the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * @package     Docalist
+ * @subpackage  Search
+ * @author      Daniel Ménard <daniel.menard@laposte.net>
+ */
+namespace Docalist\Search\Aggregation\Metrics;
+
+use Docalist\Search\Aggregation\SingleMetricAggregation;
+
+/**
+ * Une agrégation qui retourne le nombre (approximatif) de valeurs distinctes présentes dans un champ.
+ *
+ * @link https://www.elastic.co/guide/en/elasticsearch/reference/master/search-aggregations-metrics-cardinality-aggregation.html
+ */
+class CardinalityAggregation extends SingleMetricAggregation
+{
+    const TYPE = 'cardinality';
+}

d9c4bd8e76164d01e84c9079ae0f4c3881cfa779
Daniel Ménard <daniel.menard@laposte.net>
2016-08-10 19:11:27 +0200

    Nouvelle vue aggregations/range

---
+++ views/aggregations/bucket/range.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * This file is part of the 'Docalist Search' plugin.
+ *
+ * Copyright (C) 2016-2016 Daniel Ménard
+ *
+ * For copyright and license information, please view the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * @package     Docalist
+ * @subpackage  Search
+ * @author      Daniel Ménard <daniel.menard@laposte.net>
+ */
+namespace Docalist\Search\Views\Aggregation\Metrics;
+
+use Docalist\Search\Aggregation\Bucket\RangeAggregation;
+
+/**
+ * Vue par défaut pour les agrégations "range".
+ *
+ * @var RangeAggregation    $this   L'agrégation à afficher.
+ * @var string              $title  Optionnel, le titre de l'agrégation.
+ */
+if ($buckets = $this->getBuckets()) {
+    // ES génère les buckets même si le doc_count obtenu est à zéro.
+    // Comme on ne veut que les buckets pas "vides", il faut qu'on teste.
+    // Potentiellement, tous les buckets peuvent être vides, et dans ce cas, on ne veut pas générer le titre et le ul.
+    // Du coup, on génère les buckets dans une chaine et on n'affiche la facette que si on a quelque chose.
+    $items = '';
+    foreach ($buckets as $bucket) {
+        $count = $bucket->doc_count;
+        if ($count === 0) {
+            continue;
+        }
+        $label = $this->getBucketLabel($bucket);
+        $class = sprintf(
+            'range-%s-%s',
+            isset($bucket->from) ? $bucket->from : 'less-than',
+            isset($bucket->to) ? $bucket->to : 'and-more'
+        );
+        $url = '#';
+
+        $items .= sprintf(
+            '<li class="%s"><a href="%s"><strong>%s</strong> <em>%d</em></a></li>',
+            esc_attr($class), esc_attr($url), $label, $count
+        );
+    }
+
+    if ($items) {
+        printf('<h3>%s</h3>', isset($title) ? $title : $this->getName());
+        printf('<ul class="%s">%s</ul>', $this->getType(), $items);
+    }
+}

c55874c05779efb14232c7212fe4a38e128e124f
Daniel Ménard <daniel.menard@laposte.net>
2016-08-11 12:50:46 +0200

    Nouvelle aggrégation TermsIn : collection indexée

---
+++ class/Aggregation/TermsIn.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * This file is part of the 'SVB Plugin' package.
+ *
+ * Copyright (C) 2015-2016 Artwaï, Docalist
+ *
+ * For copyright and license information, please view the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * @author Daniel Ménard <daniel.menard@laposte.net>
+ */
+namespace Docalist\Search\Aggregation;
+
+use Docalist\Search\Aggregation\Bucket\TermsAggregation;
+use Docalist\Search\Indexer;
+
+/**
+ * Construit une agrégation de type "terms" sur le champ "collection indexée" (in).
+ */
+class TermsIn extends TermsAggregation
+{
+    /**
+     * Liste des indexeurs disponibles, indexés par nom de collection ('in').
+     *
+     * @var Indexer[]
+     */
+    protected $collections;
+
+    public function __construct()
+    {
+        parent::__construct('in', ['size' => 1000]);
+        $this->setViewData(['title' => 'Type de contenu']);
+    }
+
+    public function getBucketLabel($bucket)
+    {
+        // Initialise la liste des collections au premier appel
+        if (is_null($this->collections)) {
+            $this->collections = docalist('docalist-search-index-manager')->getCollections();
+        }
+
+        if (isset($this->collections[$bucket->key])) {
+            return $this->collections[$bucket->key]->getLabel();
+        }
+
+        return $bucket->key;
+    }
+}

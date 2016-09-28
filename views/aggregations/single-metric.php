84e22c357c2f8715060f627a3cc41414c7089676
Daniel Ménard <daniel.menard@laposte.net>
2016-09-28 18:15:59 +0200

    views/agg/single-metric : génère le container (même modèle que terms)

--- views/aggregations/single-metric.php
+++ views/aggregations/single-metric.php
@@ -18,12 +18,38 @@ use Docalist\Search\Aggregation\SingleMetricAggregation;
 /**
  * Vue par défaut pour les agrégations "single metric".
  *
- * @var SingleMetricAggregation $this L'agrégation à afficher.
+ * @var SingleMetricAggregation $this       L'agrégation à afficher.
+ * @var string|false            $container  Optionnel, tag à générer pour le container (div par défaut), ou false.
  */
-if ($value = $this->getValue()) {
-    printf(
-        '<span>%s</span> <em>%s</em>',
-        $this->formatValue($value),
-        $this->getTitle() ?: $this->getName()
-    );
+
+// On ne génère rien si on n'a pas de valeur
+$value = $this->getValue();
+if (empty($value)) {
+    return;
 }
+
+// Valeur par défaut des paramètres de la vue
+!isset($container) && $container = 'div';
+
+
+// Début du container
+if ($container) {
+    // Détermine les classes css à appliquer au container
+    $class = sprintf('%s %s',                                   // "facet"
+        $this->getType(),                                       // type de la facette (e.g. "facet-terms")
+        $this->getName()                                        // nom de la facette (e.g. "facet-category")
+    );
+
+    // Génère le tag ouvrant du container
+    printf('<%s class="%s">', $container, $class);
+}
+
+// Génère la valeur
+printf(
+    '<span>%s</span> <em>%s</em>',
+    $this->formatValue($value),
+    $this->getTitle() ?: $this->getName()
+);
+
+// Fin du container
+$container && printf('</%s>', $container);

ad1f7d40f3d2c6af4ab93e9d307fb69a0de2bde3
Daniel Ménard <daniel.menard@laposte.net>
2016-09-28 18:19:07 +0200

    views/agg/stats : génère le container (même modèle que terms)

--- views/aggregations/metrics/stats.php
+++ views/aggregations/metrics/stats.php
@@ -18,20 +18,45 @@ use Docalist\Search\Aggregation\Metrics\StatsAggregation;
 /**
  * Vue par défaut pour les agrégations "stats".
  *
- * @var StatsAggregation $this L'agrégation à afficher.
+ * @var StatsAggregation    $this       L'agrégation à afficher.
+ * @var string|false        $container  Optionnel, tag à générer pour le container (div par défaut), ou false.
  */
-if ($count = $this->getCount()) {
-    $details = sprintf('%s fiche(s), min %s, max %s, moyenne %s',
-        (string) $count,
-        $this->formatValue($this->getMin()),
-        $this->formatValue($this->getMax()),
-        $this->formatValue($this->getAvg())
-    );
 
-    printf(
-        '<span title="%s">%s</span> <em>%s</em>',
-        esc_attr($details),
-        $this->formatValue($this->getSum()),
-        $this->getTitle() ?: $this->getName()
-    );
+// On ne génère rien si on n'a pas de stats
+$count = $this->getCount();
+if (empty($count)) {
+    return;
 }
+
+// Valeur par défaut des paramètres de la vue
+!isset($container) && $container = 'div';
+
+// Début du container
+if ($container) {
+    // Détermine les classes css à appliquer au container
+    $class = sprintf('%s %s',                                   // "facet"
+        $this->getType(),                                       // type de la facette (e.g. "facet-terms")
+        $this->getName()                                        // nom de la facette (e.g. "facet-category")
+        );
+
+    // Génère le tag ouvrant du container
+    printf('<%s class="%s">', $container, $class);
+}
+
+// Génère les stats
+$details = sprintf('%s fiche(s), min %s, max %s, moyenne %s',
+    (string) $count,
+    $this->formatValue($this->getMin()),
+    $this->formatValue($this->getMax()),
+    $this->formatValue($this->getAvg())
+);
+
+printf(
+    '<span title="%s">%s</span> <em>%s</em>',
+    esc_attr($details),
+    $this->formatValue($this->getSum()),
+    $this->getTitle() ?: $this->getName()
+);
+
+// Fin du container
+$container && printf('</%s>', $container);

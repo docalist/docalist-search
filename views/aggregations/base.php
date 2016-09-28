be162a7f2b8c486b40c7411610af43286ccbaae1
Daniel Ménard <daniel.menard@laposte.net>
2016-09-28 18:08:12 +0200

    views/agg/base : génère le container (même modèle que terms)

--- views/aggregations/base.php
+++ views/aggregations/base.php
@@ -18,10 +18,32 @@ use Docalist\Search\Aggregation;
 /**
  * Vue par défaut pour les agrégations.
  *
- * @var Aggregation $this L'agrégation à afficher.
+ * @var Aggregation     $this       L'agrégation à afficher.
+ * @var string|false    $container  Optionnel, tag à générer pour le container (div par défaut), ou false.
  */
+
+// Valeur par défaut des paramètres de la vue
+!isset($container) && $container = 'div';
+
+// Début du container
+if ($container) {
+    // Détermine les classes css à appliquer au container
+    $class = sprintf('%s %s',                                   // "facet"
+        $this->getType(),                                       // type de la facette (e.g. "facet-terms")
+        $this->getName()                                        // nom de la facette (e.g. "facet-category")
+    );
+
+    // Génère le tag ouvrant du container
+    printf('<%s class="%s">', $container, $class);
+
+}
+
+// Dump de l'agrégation
 printf(
     '<h3>%s</h3><pre>%s</pre>',
     $this->getTitle() ?: $this->getName(),
     json_encode($this->getResult(), JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT)
 );
+
+// Fin du container
+$container && printf('</%s>', $container);

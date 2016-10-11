d02808aad73cc80aaadb8a9d298ca4d1ad9ca58b
Daniel Ménard <daniel.menard@laposte.net>
2016-10-11 19:06:55 +0200

    Nouvelle classe TermsCategory.
    Une agrégation de type "terms" sur la taxonomie "catégorie".

---
+++ class/Aggregation/Standard/TermsCategory.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * This file is part of the 'SVB Plugin' package.
+ *
+ * Copyright (C) 2015-2016 Artwaï, Docalist
+ *
+ * For copyright and license information, please view the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * @author Daniel Ménard <daniel.menard@laposte.net>
+ */
+namespace Docalist\Search\Aggregation\Standard;
+
+use Docalist\Search\Aggregation\Bucket\TaxonomyEntriesAggregation;
+
+/**
+ * Construit une agrégation de type "terms" sur le champ "section du site" (topic-section.filter).
+ */
+class TermsCategory extends TaxonomyEntriesAggregation
+{
+    public function __construct()
+    {
+        parent::__construct('category', 'category', ['size' => 1000/*, 'missing' => self::MISSING*/]);
+        $this->setTitle('Catégorie');
+    }
+}

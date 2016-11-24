26586b5b9b0f09bda5e12a8857d962544c9bb1e2
Daniel Ménard <daniel.menard@laposte.net>
2016-11-24 11:34:49 +0100

    MetricsAggregation : adaptation et amélioration
    - Nouveau paramètre $renderOptions (optionnel) dans le constructeur.
    - On a maintenant plusieurs options de rendu qui permettent de
    personnaliser le formattage des métriques affichées (nombre de chiffres
    après la virgule, format, etc.)
    - Améliore la méthode formatValue() pour tenir compte des options.

--- class/Aggregation/MetricsAggregation.php
+++ class/Aggregation/MetricsAggregation.php
@@ -23,16 +23,37 @@ abstract class MetricsAggregation extends BaseAggregation
     /**
      * Constructeur
      *
-     * @param string    $field      Champ sur lequel porte l'agrégation.
-     * @param array     $parameters Autres paramètres de l'agrégation.
+     * @param string    $field          Champ sur lequel porte l'agrégation.
+     * @param array     $parameters     Autres paramètres de l'agrégation.
+     * @param array     $renderOptions  Options d'affichage.
      */
-    public function __construct($field, array $parameters = [])
+    public function __construct($field, array $parameters = [], array $renderOptions = [])
     {
-        parent::__construct(['field' => $field] + $parameters);
+        $parameters['field'] = $field;
+        parent::__construct($parameters, $renderOptions);
+    }
+
+    public function getDefaultRenderOptions()
+    {
+        $options = parent::getDefaultRenderOptions();
+        $options['title.tag'] = 'em';
+        $options['title.before'] = false;
+        $options['content.tag']  = 'span';
+        $options['metric.decimals']  = 2; // Nombre de chiffres après la virgule.
+        $options['metric.point']  = ','; // Caractère utilisé pour le point décimal.
+        $options['metric.thousands']  = ' '; // Séparateur de milliers (espace insécable par défaut).
+        $options['metric.format']  = '%s'; // Format final (exemple : '%s €' ou '$%s').
+        $options['metric.zero']  = false; // Affiche ou non les valeurs zéro.
+
+        // On pourrait utiliser :
+        // metric.point     = $wp_locale->number_format['decimal_point']
+        // metric.thousands = $wp_locale->number_format['thousands_sep']
+
+        return $options;
     }
 
     /**
-     * Format la valeur passée en paramètre.
+     * Formatte la valeur passée en paramètre.
      *
      * @param int|float $value
      *
@@ -40,6 +61,23 @@ abstract class MetricsAggregation extends BaseAggregation
      */
     public function formatValue($value)
     {
-        return (string) $value;
+        // Arrondit la valeur au nombre de chiffres après la virgule qui figure dans les options.
+        $value = round($value, $this->renderOptions['metric.decimals']);
+
+        // On ne génère rien si la valeur est à 0 et que l'option 'metric.zero' est à false
+        if (0 == $value && !$this->renderOptions['metric.zero']) {
+            return '';
+        }
+
+        // Formatte le nombre en fonction des options d'affichage
+        $value = number_format(
+            $value,
+            ($value == (int) $value) ? 0 : $this->renderOptions['metric.decimals'],
+            $this->renderOptions['metric.point'],
+            $this->renderOptions['metric.thousands']
+        );
+
+        // Retourne la valeur formattée
+        return sprintf($this->renderOptions['metric.format'], $value);
     }
 }

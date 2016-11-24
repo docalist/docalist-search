01dde770e9f4cc3a5ab3aaba509028a33cbe1af6
Daniel Ménard <daniel.menard@laposte.net>
2016-11-24 15:19:40 +0100

    MultiBucketsAggregation : API standard pour l'affichage des buckets
    - surcharge la méthode getDefaultRenderOptions() pour ajouter de
    nouvelles options de rendu
    - surcharge la méthode renderResult() : elle se contente d'appeller
    renderBuckets()
    - nouvelle méthode renderBuckets() qui affiche les buckets pour un
    niveau donné
    - nouvelle méthode renderBucket() qui affiche le bucket, ses
    sous-agrégations et récursive sur renderBuckets() si le bucket a des
    buckets enfants.

--- class/Aggregation/MultiBucketsAggregation.php
+++ class/Aggregation/MultiBucketsAggregation.php
@@ -13,6 +13,8 @@
  */
 namespace Docalist\Search\Aggregation;
 
+use stdClass;
+
 /**
  * Classe de base pour les agrégations de type "bucket" qui retournent plusieurs buckets.
  *
@@ -20,4 +22,88 @@ namespace Docalist\Search\Aggregation;
  */
 abstract class MultiBucketsAggregation extends BucketAggregation
 {
+    public function getDefaultRenderOptions()
+    {
+        $options = parent::getDefaultRenderOptions();
+
+        $options['container.css']  = 'facet';
+        $options['content.tag']  = 'ul';
+
+        $options['bucket.tag']  = 'li';             // Tag à utiliser pour les bucket
+        $options['bucket.label.tag']  = 'span';     // Tag à utiliser pour le libellé des buckets
+        $options['bucket.count.tag']  = 'em';       // Tag à utiliser pour le count des buckets ou false
+
+        return $options;
+    }
+
+    protected function renderResult()
+    {
+        return $this->renderBuckets($this->getBuckets());
+    }
+
+    /**
+     * Génère le rendu des buckets passés en paramètre.
+     *
+     * @param stdClass[] $buckets
+     *
+     * @return string
+     */
+    protected function renderBuckets(array $buckets)
+    {
+        $result = '';
+        foreach ($buckets as $bucket) {
+            $bucket = $this->prepareBucket($bucket);
+            $bucket && $result .= $this->renderBucket($bucket);
+        }
+
+        return $result;
+    }
+
+    /**
+     * Génère le rendu du bucket passé en paramètre.
+     *
+     * @param stdClass $bucket
+     *
+     * @return string
+     */
+    protected function renderBucket(stdClass $bucket)
+    {
+        // Génère le libellé du bucket
+        $tag = $this->renderOptions['bucket.label.tag'];
+        $result = $this->renderTag($tag, [], $this->getBucketLabel($bucket));
+
+        // Génère le nombre de documents obtenus pour ce bucket
+        $tag = $this->renderOptions['bucket.count.tag'];
+        $tag && $result .= $this->renderTag($tag, [], $bucket->doc_count);
+
+        // Génère le lien permettant d'activer ou de désativer ce bucket
+        $field = $this->getParameter('field');
+        $searchUrl = $this->getSearchRequest()->getSearchUrl();
+        $url = $searchUrl->toggleFilter($field, $bucket->key);
+        $result = $this->renderTag('a', ['href' => $url], $result);
+
+        // Génère les sous-agrégations de ce bucket
+        foreach($this->getAggregations() as $aggregation) {
+            $result .= $aggregation->render(['container' => false, 'title' => false]);
+        }
+
+        // Génère les buckets enfants de ce bucket (s'il s'agit d'une hiérarchie)
+        if (!empty($bucket->children)) {
+            if ($children = $this->renderBuckets($bucket->children)) {
+                $tag = $this->renderOptions['content.tag'];
+                $result .= $this->renderTag($tag, [], $children);
+            }
+        }
+
+        // Génère les attributs du bucket
+        $attributes = ['class' => $bucket->key];
+        $searchUrl->hasFilter($field, $bucket->key) && $attributes['class'] .= ' filter-active';
+        if ($this->renderOptions['data']) {
+            $attributes['data-bucket'] = $bucket->key;
+            $attributes['data-count'] = $bucket->doc_count;
+        }
+
+        // Génère le bucket
+        return $this->renderTag($this->renderOptions['bucket.tag'], $attributes, $result);
+    }
 }

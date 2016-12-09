17797a0f1e365becf91d62c45af97bef522d70b7
Daniel Ménard <daniel.menard@laposte.net>
2016-12-09 18:50:08 +0100

    FilterAggregation : surcharge la méthode render() héritée.
    Une agrégation de type 'filter' ne génère rien elle-même : elle se
    contente d'afficher les sous-agrégations qu'elle contient. 
    
    Du coup, elle n'a pas de container, pas de titre, etc.
    
    Donc on surcharge la méthode render() héritée de BaseAggregation pour
    court-circuiter complètement le traitement par défaut.

--- class/Aggregation/Bucket/FilterAggregation.php
+++ class/Aggregation/Bucket/FilterAggregation.php
@@ -36,4 +36,34 @@ class FilterAggregation extends SingleBucketAggregation
         $parameters = $filter;
         parent::__construct($parameters, $options);
     }
+
+    public function render(array $options = [])
+    {
+        /*
+         * Une agrégation de type 'filter' ne génère rien elle-même : elle se contente d'afficher
+         * les sous-agrégations qu'elle contient. Du coup, elle n'a pas de container, pas de titre, etc.
+         * Donc on surcharge la méthode render() héritée de BaseAggregation pour court-circuiter
+         * complètement le traitement par défaut.
+         * Important : les options d'affichage passées à render() s'appliquent aux sous-agrégations, pas à
+         * l'agrégation globale.
+         */
+
+        // On ne génère rien si on n'a pas de résultat ou si on n'a aucune sous-agrégation
+        if (is_null($this->result) || !$this->hasAggregations()) {
+            return '';
+        }
+
+        // Prépare le bucket (transmet leur résultat aux sous-agrégations)
+        $this->prepareBucket($this->result);
+
+        // Génère chacune des sous-agrégations
+        $result = '';
+        foreach($this->getAggregations() as $aggregation) {
+            $result .= $aggregation->render($options);
+        }
+
+        // Ok
+        return $result;
+    }
+
 }

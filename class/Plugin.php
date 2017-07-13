554d0a5e416ec5d062d86d7c73debe64c594d21c
Daniel Ménard <daniel.menard@laposte.net>
2017-07-13 09:09:56 +0200

    Update copyright.

--- class/Plugin.php
+++ class/Plugin.php
@@ -2,7 +2,7 @@
 /**
  * This file is part of the "Docalist Search" plugin.
  *
- * Copyright (C) 2012-2015 Daniel Ménard
+ * Copyright (C) 2012-2017 Daniel Ménard
  *
  * For copyright and license information, please view the
  * LICENSE.txt file that was distributed with this source code.

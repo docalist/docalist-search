14b8180f861636557a1c2749946608f0fcfdacb1
Daniel Ménard <daniel.menard@laposte.net>
2017-07-17 10:57:28 +0200

    SearchEngine : le champ "title" a été renommé "posttitle", adaptation.
    cf. docalist-biblio 400a2167025d56b56ec22a6e608515c5c739d31e

--- class/SearchEngine.php
+++ class/SearchEngine.php
@@ -2,7 +2,7 @@
 /**
  * This file is part of the "Docalist Search" plugin.
  *
- * Copyright (C) 2012-2016 Daniel Ménard
+ * Copyright (C) 2012-2017 Daniel Ménard
  *
  * For copyright and license information, please view the
  * LICENSE.txt file that was distributed with this source code.
@@ -121,8 +121,8 @@ class SearchEngine
             case 'lastupdate-':     return ['lastupdate' => 'desc'];
 
             // Titre
-            case 'title':           return 'title-sort';
-            case 'title-':          return ['title-sort' => 'desc'];
+            case 'posttitle':       return 'posttitle-sort';
+            case 'posttitle-':      return ['posttitle-sort' => 'desc'];
         }
 
         return null; // tri non reconnu
@@ -145,8 +145,8 @@ class SearchEngine
             case 'lastupdate-': return 'Date de mise à jour (récent -> ancien)';
 
             // Titre
-            case 'title':       return 'Titre (A -> Z)';
-            case 'title-':      return 'Titre (Z -> A)';
+            case 'posttitle':   return 'Titre (A -> Z)';
+            case 'posttitle-':  return 'Titre (Z -> A)';
         }
 
         return $sort; // tri non reconnu

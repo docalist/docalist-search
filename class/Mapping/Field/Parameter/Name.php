6280586b2b23bfdc3245430a445b6aff77036a76
Daniel Ménard <daniel.menard@laposte.net>
2019-04-24 19:58:54 +0200

    Interface Name : gère le nom d'un champ de mapping.

---
+++ class/Mapping/Field/Parameter/Name.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * This file is part of Docalist Search.
+ *
+ * Copyright (C) 2012-2019 Daniel Ménard
+ *
+ * For copyright and license information, please view the
+ * LICENSE file that was distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Docalist\Search\Mapping\Field\Parameter;
+
+use Docalist\Search\Mapping\Options;
+
+/**
+ * Gère le nom d'un champ de mapping.
+ *
+ * @author Daniel Ménard <daniel.menard@laposte.net>
+ */
+interface Name
+{
+    /**
+     * Retourne le nom du champ.
+     *
+     * @return string
+     */
+    public function getName(): string;
+
+    // Remarque : setName() est protected, on ne peut pas changer le nom d'un champ.
+    // Elle est implémentée dans le trait mais elle ne figure pas dans l'interface.
+}

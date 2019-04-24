926ee575ea7a87b6e70af636f9eb5e5fa76202e7
Daniel Ménard <daniel.menard@laposte.net>
2019-04-24 16:31:18 +0200

    Nouvelle interface Analysis/Component
    - Début des commits du nouveau module "docalist-search-analysis"
    - Ré-écriture sous forme de classes des composants d'analyse utilisés
    pour définir les mappings elastic-search (analyseurs, char filters,
    tokenizers, filtre de tokens)
    - Auparavant, on manipulait les composants sous la forme de tableaux de
    tableaux qu'on incluait dans le code à coup de "require" (dans
    /index-settings)
    - Le module Analysis est complètement indépendant (ça pourrait être une
    librairie séparée).
    - L'interface Analysis/Component est l'interface la plus générique des
    composants d'analyse : elle dit simplement qu'un composant a un nom et
    une définition.

---
+++ class/Analysis/Component.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * This file is part of Docalist Search.
+ *
+ * Copyright (C) 2012-2019 Daniel Ménard
+ *
+ * For copyright and license information, please view the
+ * LICENSE file that was distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Docalist\Search\Analysis;
+
+/**
+ * Interface d'un composant d'analyse.
+ *
+ * @link https://www.elastic.co/guide/en/elasticsearch/reference/current/analyzer-anatomy.html
+ *
+ * @author Daniel Ménard <daniel.menard@laposte.net>
+ */
+interface Component
+{
+    /**
+     * Retourne le nom du composant.
+     *
+     * @return string
+     */
+    public static function getName(): string;
+
+    /**
+     * Retourne la définition du composant.
+     *
+     * @return array
+     */
+    public function getDefinition(): array;
+}

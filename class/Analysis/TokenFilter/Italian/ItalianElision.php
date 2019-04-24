88ca8f5729c80140264ef190f3b34dafcb4da365
Daniel Ménard <daniel.menard@laposte.net>
2019-04-24 17:20:02 +0200

    TokenFilter "italian_elision" : supprime les élisions en italien.

---
+++ class/Analysis/TokenFilter/Italian/ItalianElision.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * This file is part of Docalist Search.
+ *
+ * Copyright (C) 2012-2019 Daniel Ménard
+ *
+ * For copyright and license information, please view the
+ * LICENSE file that was distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Docalist\Search\Analysis\TokenFilter\Italian;
+
+use Docalist\Search\Analysis\TokenFilter;
+
+/**
+ * TokenFilter "italian_elision" : supprime les élisions en italien.
+ *
+ * @link https://github.com/apache/lucene-solr/blob/master/lucene/analysis/common/src/java/org/apache/lucene/
+ * analysis/it/ItalianAnalyzer.java#L53
+ *
+ * @author Daniel Ménard <daniel.menard@laposte.net>
+ */
+class ItalianElision implements TokenFilter
+{
+    /**
+     * {@inheritDoc}
+     */
+    public static function getName(): string
+    {
+        return 'italian_elision';
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getDefinition(): array
+    {
+        return [
+            'type' => 'elision',
+            'articles' => [
+                'c', 'l', 'all', 'dall', 'dell', 'nell', 'sull', 'coll', 'pell',
+                'gl', 'agl', 'dagl', 'degl', 'negl', 'sugl', 'un', 'm', 't', 's', 'v', 'd',
+            ],
+        ];
+    }
+}

3f9d4c6289bd686ac93eba6f08c30de74b349b55
Daniel Ménard <daniel.menard@laposte.net>
2019-04-24 20:00:02 +0200

    LabelTrait : implémentation de l'interface Label.

---
+++ class/Mapping/Field/Info/LabelTrait.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * This file is part of Docalist Search.
+ *
+ * Copyright (C) 2012-2019 Daniel Ménard
+ *
+ * For copyright and license information, please view the
+ * LICENSE file that was distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Docalist\Search\Mapping\Field\Info;
+
+use Docalist\Search\Mapping\Field\Info\Label;
+
+/**
+ * Implémentation de l'interface Label.
+ *
+ * @author Daniel Ménard <daniel.menard@laposte.net>
+ */
+trait LabelTrait
+{
+    /**
+     * Valeur du paramètre.
+     *
+     * @var string
+     */
+    private $label = '';
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setLabel(string $label): self
+    {
+        $this->label = $label;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getLabel(): string
+    {
+        return $this->label;
+    }
+
+    /**
+     * Fusionne avec un autre Label.
+     *
+     * @param Label $other
+     */
+    protected function mergeLabel(Label $other): void
+    {
+        $label = $other->getLabel();
+        if ($label !== $this->label) {
+            $this->label = trim($this->label . ' ' . $label);
+        }
+    }
+}

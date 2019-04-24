421ea0407f80f3d2d8522f794a657d01e2ba75d5
Daniel Ménard <daniel.menard@laposte.net>
2019-04-24 17:37:51 +0200

    Analyseur "english_text".
    Permet une recherche "plein texte" sur un contenu en anglais (titre,
    résumé...)

---
+++ class/Analysis/Analyzer/EnglishText.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * This file is part of Docalist Search.
+ *
+ * Copyright (C) 2012-2019 Daniel Ménard
+ *
+ * For copyright and license information, please view the
+ * LICENSE file that was distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Docalist\Search\Analysis\Analyzer;
+
+use Docalist\Search\Analysis\Analyzer\CustomAnalyzer;
+use Docalist\Search\Analysis\TokenFilter\English\EnglishPossessives;
+use Docalist\Search\Analysis\TokenFilter\English\EnglishStop;
+use Docalist\Search\Analysis\TokenFilter\English\EnglishStem;
+
+/**
+ * Analyseur "english_text" : permet une recherche "plein texte" sur un contenu en anglais (titre, résumé...)
+ *
+ * @author Daniel Ménard <daniel.menard@laposte.net>
+ */
+class EnglishText extends CustomAnalyzer
+{
+    /**
+     * {@inheritDoc}
+     */
+    public static function getName(): string
+    {
+        return 'english_text';
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getCharFilters(): array
+    {
+        return [
+            'html_strip',           // Supprime les tags html
+        ];
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getTokenFilters(): array
+    {
+        return [
+            'lowercase',                    // Convertit le texte en minuscules
+            EnglishPossessives::getName(),  // Supprime les "'s" à la fin des mots
+            EnglishStop::getName(),         // Supprime les mots-vides
+            'asciifolding',                 // Supprime les accents
+            EnglishStem::getName(),         // Stemming standard
+        ];
+
+        // Remarques :
+        // - Il y a des accents en anglais, donc asciifolding (http://en.wikipedia.org/wiki/Diacritic#English)
+        // - Stop doit être exécuté après lowercase car les mots vides sont en minuscule dans la liste.
+        //   Je l'ai mis avant asciifolding pour être cohérent avec ce que font les autres langues.
+    }
+}

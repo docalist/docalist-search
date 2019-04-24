b8c915a9dc1c6d79359eb127fb81d66c76080408
Daniel Ménard <daniel.menard@laposte.net>
2019-04-24 17:06:10 +0200

    TokenFilter "french_elision" : supprime les élisions en français.

---
+++ class/Analysis/TokenFilter/French/FrenchElision.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * This file is part of Docalist Search.
+ *
+ * Copyright (C) 2012-2019 Daniel Ménard
+ *
+ * For copyright and license information, please view the
+ * LICENSE file that was distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Docalist\Search\Analysis\TokenFilter\French;
+
+use Docalist\Search\Analysis\TokenFilter;
+
+/**
+ * TokenFilter "french_elision" : supprime les élisions en français.
+ *
+ * @link https://github.com/apache/lucene-solr/blob/master/lucene/analysis/common/src/java/org/apache/lucene/
+ * analysis/fr/FrenchAnalyzer.java#L63
+ *
+ * @author Daniel Ménard <daniel.menard@laposte.net>
+ */
+class FrenchElision implements TokenFilter
+{
+    /**
+     * {@inheritDoc}
+     */
+    public static function getName(): string
+    {
+        return 'french_elision';
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getDefinition(): array
+    {
+        return [
+            'type' => 'elision',
+            'articles' => [
+                'l', 'm', 't', 'qu', 'n', 's','j', 'd', 'c', 'jusqu', 'quoiqu','lorsqu', 'puisqu',
+            ],
+        ];
+    }
+}

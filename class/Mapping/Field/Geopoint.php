c6942ad96d1ebc4d0b7390fdec645e01c713051a
Daniel Ménard <daniel.menard@laposte.net>
2019-04-24 19:53:52 +0200

    GeopointField : un point géographique

---
+++ class/Mapping/Field/Geopoint.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * This file is part of Docalist Search.
+ *
+ * Copyright (C) 2012-2019 Daniel Ménard
+ *
+ * For copyright and license information, please view the
+ * LICENSE file that was distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Docalist\Search\Mapping\Field;
+
+use Docalist\Search\Mapping\Field;
+use Docalist\Search\Mapping\Options;
+
+/**
+ * Un point géographique défini par une latitude et une longitude.
+ *
+ * @link https://www.elastic.co/guide/en/elasticsearch/reference/master/geo-point.html
+ *
+ * @author Daniel Ménard <daniel.menard@laposte.net>
+ */
+final class GeopointField extends Field
+{
+    /**
+     * {@inheritDoc}
+     */
+    final public function getMapping(Options $options): array
+    {
+        // Génère le mapping de base
+        $mapping = parent::getMapping($options);
+
+        // Type de champ
+        $mapping['type'] = 'geo_point';
+        $mapping['ignore_malformed'] = true;
+
+        // Ok
+        return $mapping;
+    }
+}

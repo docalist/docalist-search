e623b84cc33468438859c2ca632ca817ea5e5a96
Daniel Ménard <daniel.menard@laposte.net>
2019-04-25 11:51:06 +0200

    ShortField : un entier signé sur 16 bits.

---
+++ class/Mapping/Field/ShortField.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * This file is part of Docalist Search.
+ *
+ * Copyright (C) 2012-2019 Daniel Ménard
+ *
+ * For copyright and license information, please view the
+ * LICENSE file that was distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Docalist\Search\Mapping\Field;
+
+use Docalist\Search\Mapping\Field\NumericField;
+
+/**
+ * Un entier signé sur 16 bits compris entre -32768 et 32767.
+ *
+ * @link https://www.elastic.co/guide/en/elasticsearch/reference/master/number.html
+ *
+ * @author Daniel Ménard <daniel.menard@laposte.net>
+ */
+final class ShortField extends NumericField
+{
+    /**
+     * {@inheritDoc}
+     */
+    final protected function getNumericType(): string
+    {
+        return 'short';
+    }
+}

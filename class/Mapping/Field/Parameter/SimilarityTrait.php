9dc8f4a7aa1dd2e64e4d9b1a96e3ac186f8a13f4
Daniel Ménard <daniel.menard@laposte.net>
2019-04-25 08:34:07 +0200

    SimilarityTrait : implémentation de l'interface Similarity.

---
+++ class/Mapping/Field/Parameter/SimilarityTrait.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * This file is part of Docalist Search.
+ *
+ * Copyright (C) 2012-2019 Daniel Ménard
+ *
+ * For copyright and license information, please view the
+ * LICENSE file that was distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Docalist\Search\Mapping\Field\Parameter;
+
+use Docalist\Search\Mapping\Field\Parameter\Similarity;
+use Docalist\Search\Mapping\Options;
+use InvalidArgumentException;
+
+/**
+ * Implémentation de l'interface Similarity.
+ *
+ * @author Daniel Ménard <daniel.menard@laposte.net>
+ */
+trait SimilarityTrait
+{
+    /**
+     * Valeur du paramètre.
+     *
+     * @var string
+     */
+    private $similarity = Similarity::DEFAULT_SIMILARITY;
+
+    /**
+     * {@inheritDoc}
+     */
+    final public function setSimilarity(string $similarity): self
+    {
+        $this->similarity = $similarity;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    final public function getSimilarity(): string
+    {
+        return $this->similarity;
+    }
+
+    /**
+     * Fusionne avec un autre Similarity.
+     *
+     * @param Similarity $other
+     */
+    final protected function mergeSimilarity(Similarity $other): void
+    {
+        if ($other->getSimilarity() !== $this->similarity) {
+            throw new InvalidArgumentException(sprintf(
+                'similarity mismatch (%s vs %s)',
+                var_export($other->getSimilarity(), true),
+                var_export($this->similarity, true)
+            ));
+        }
+    }
+
+    /**
+     * Applique le paramètre au mapping.
+     *
+     * @param array     $mapping    Mapping à modifier.
+     * @param Options   $options    Options du mapping.
+     */
+    final protected function applySimilarity(array & $mapping, Options $options): void
+    {
+        !empty($this->similarity) && $mapping['similarity'] = $this->similarity;
+    }
+}

28b44958846ca00e6026204e801d0b29217ce615
Daniel Ménard <daniel.menard@laposte.net>
2019-04-25 08:31:12 +0200

    NormalizerTrait : implémentation de l'interface Normalizer.

---
+++ class/Mapping/Field/Parameter/NormalizerTrait.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * This file is part of Docalist Search.
+ *
+ * Copyright (C) 2012-2019 Daniel Ménard
+ *
+ * For copyright and license information, please view the
+ * LICENSE file that was distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Docalist\Search\Mapping\Field\Parameter;
+
+use Docalist\Search\Mapping\Field\Parameter\Normalizer;
+use Docalist\Search\Mapping\Options;
+use InvalidArgumentException;
+
+/**
+ * Implémentation de l'interface Normalizer.
+ *
+ * @author Daniel Ménard <daniel.menard@laposte.net>
+ */
+trait NormalizerTrait
+{
+    /**
+     * Valeur du paramètre.
+     *
+     * @var string
+     */
+    private $normalizer = '';
+
+    /**
+     * {@inheritDoc}
+     */
+    final public function setNormalizer(string $normalizer): self
+    {
+        $this->normalizer = $normalizer;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    final public function getNormalizer(): string
+    {
+        return $this->normalizer;
+    }
+
+    /**
+     * Fusionne avec un autre Normalizer.
+     *
+     * @param Normalizer $other
+     */
+    final protected function mergeNormalizer(Normalizer $other): void
+    {
+        if ($other->getNormalizer() !== $this->normalizer) {
+            throw new InvalidArgumentException(sprintf(
+                'normalizer mismatch (%s vs %s)',
+                var_export($other->getNormalizer(), true),
+                var_export($this->normalizer, true)
+            ));
+        }
+    }
+
+    /**
+     * Applique le paramètre au mapping.
+     *
+     * @param array     $mapping    Mapping à modifier.
+     * @param Options   $options    Options du mapping.
+     */
+    final protected function applyNormalizer(array & $mapping, Options $options): void
+    {
+        !empty($this->normalizer) && $mapping['normalizer'] = $this->normalizer;
+    }
+}

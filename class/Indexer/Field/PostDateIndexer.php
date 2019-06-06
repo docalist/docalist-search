7bee63a14a1db59f957cd11172ed5a474a3d1d8f
Daniel Ménard <daniel.menard@laposte.net>
2019-06-06 22:57:31 +0200

    PostDateIndexer : simplifie l'indexation des dates
    Supprime la recherche par expression (même traitement que dans la classe
    DateFieldIndexer de docalist-data).

--- class/Indexer/Field/PostDateIndexer.php
+++ class/Indexer/Field/PostDateIndexer.php
@@ -12,6 +12,7 @@ declare(strict_types=1);
 namespace Docalist\Search\Indexer\Field;
 
 use Docalist\Search\Mapping;
+use Docalist\Search\Mapping\Field\Parameter\IndexOptions;
 use DateTime;
 
 /**
@@ -51,30 +52,19 @@ class PostDateIndexer // pas final, surchargée par PostModifiedIndexer
     {
         $mapping
             ->literal(static::SEARCH_FIELD)
+            ->setIndexOptions(IndexOptions::INDEX_DOCS)
             ->setFeatures(Mapping::FULLTEXT)
             ->setLabel(__(
                 "Recherche sur la date de création du post WordPress ou de la référence docalist.",
                 'docalist-search'
             ))
-            ->setDescription(strtr(
+            ->setDescription(
                 __(
-                    'Exemples :
-                    <code>{field}:{year}</code>,
-                    <code>{field}:{year}{month}</code>,
-                    <code>{field}:{year}{month}{day}</code>,
-                    <code>{field}:"{year}/{month}"</code>,
-                    <code>{field}:"{month}/{year}"</code>,
-                    <code>{field}:"{year}/{month}/{day}"</code>,
-                    <code>{field}:"{day}/{month}/{year}"</code>.',
+                    'Chaque date est indexée sous trois formes : yyyy (année uniquement), yyyymm (année et mois)
+                    et yyyymmdd (année, mois et jour).',
                     'docalist-search'
-                ),
-                [
-                    '{field}' => static::SEARCH_FIELD,
-                    '{year}' => date('Y'),
-                    '{month}' => date('m'),
-                    '{day}' => date('d'),
-                ]
-            ));
+                )
+            );
 
         $mapping
             ->date(static::DATE_FILTER)
@@ -119,10 +109,8 @@ class PostDateIndexer // pas final, surchargée par PostModifiedIndexer
         $year = $dateTime->format('Y');             // Année ("2019")
         $yearMonth = $dateTime->format('Ym');       // Année + mois ("201904")
         $yearMonthDay = $dateTime->format('Ymd');   // Année + mois + jour ("20190408")
-        $phrase1 = $dateTime->format('Y m d');      // Recherche par phrase ordre y m d ("2019 04 08")
-        $phrase2 = $dateTime->format('d m Y');      // Recherche par phrase ordre y m d ("08 04 2019")
 
-        $data[static::SEARCH_FIELD] = [$year, $yearMonth, $yearMonthDay, $phrase1, $phrase2];
+        $data[static::SEARCH_FIELD] = $year . ' ' . $yearMonth . ' ' . $yearMonthDay;
         $data[static::DATE_FILTER] = $date;
         $data[static::HIERARCHY_FILTER] = $dateTime->format('Y/m/d');
     }
